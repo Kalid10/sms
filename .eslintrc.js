a539604983cfcff2e3691ae60a6ff5c831a87008
Kalid Abdu <Kalidabdu75@gmail.com>
2023-03-14 19:43:46 +0300

    Install eslint and prettier - Run npm install - Run npm run lint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "plugin:vue/vue3-recommended",
+        "plugin:tailwindcss/recommended",
+        "prettier",
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["vue"],
+    rules: {
+        "vue/multi-word-component-names": "off",
+        "vue/no-v-text-v-html-on-component": "off",
+        "vue/no-setup-props-destructure": "off",
+        "tailwindcss/no-custom-classname": "off",
+        "vue/no-template-shadow": "off",
+    },
+};
