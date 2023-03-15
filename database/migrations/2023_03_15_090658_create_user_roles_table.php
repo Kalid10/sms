ea4fe00b0e5a0d4b114bc6098b55ad1a87ac467d
Kalid Abdu <Kalidabdu75@gmail.com>
2023-03-15 12:14:08 +0300

    Add role tables, models, commands and controllers

---
+++ database/migrations/2023_03_15_090658_create_user_roles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->string('role_name', 50);
+            $table->foreign('role_name')->references('name')->on('roles');
+            $table->foreignId('user_id')->constrained();
+            $table->primary(['role_name', 'user_id']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_roles');
+    }
+};
