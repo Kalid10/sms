e171fcf2c926b62248333cd55e1201e032cdbc3a
adilabdu <adilabdu68@gmail.com>
2023-03-15 12:55:52 +0300

    Setup project-wide layout inside `Layout` component
    - New `Drawer` and `Header` view components called inside the layout

---
+++ resources/js/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div class="relative flex h-screen w-full flex-col">
+        <Header @open-drawer="drawerVisible = true"/>
+        <Drawer v-model:visible="drawerVisible"/>
+        <div class="grow overflow-y-auto bg-neutral-50 p-2">
+            <slot/>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+import {ref} from "vue";
+import Header from "@/Views/Header.vue";
+import Drawer from "@/Views/Drawer.vue";
+
+const drawerVisible = ref(false);
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Views/Header.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <header class="relative z-10 flex min-h-10 w-full items-center bg-white px-2 shadow-sm">
+        <Bars3Icon class="h-6 w-6 stroke-2 text-neutral-700" @click="$emit('openDrawer')"/>
+    </header>
+
+</template>
+
+<script setup>
+import {Bars3Icon} from "@heroicons/vue/24/outline"
+
+const emits = defineEmits(['openDrawer'])
+</script>
+
+<style scoped>
+
+</style>
