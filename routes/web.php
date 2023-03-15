60c1101294e846554fa08d7fbd7324ee19554265
adilabdu <adilabdu68@gmail.com>
2023-03-15 16:48:59 +0300

    Define new route and SFC page for users

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 });
 
 Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/users', function () {
+    return Inertia::render('Users/Index');
+});
