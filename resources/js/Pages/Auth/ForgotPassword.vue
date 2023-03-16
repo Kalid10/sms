f45242410886f274c89c02eead75930eee1fdd91
Biniyam Lemma <lemmasbini@gmail.com>
2023-03-16 11:41:33 +0300

    -Change transition from forgot password to reset password `ForgotPassword` page. -Add new illustrations to the `login` page and integrate to the login api.

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,68 +1,105 @@
 <template>
-    <div class="bg-gray-50 dark:bg-gray-900">
-        <div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-            <a href="#" class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
-                Forget Password ?
-            </a>
-            <div class="w-full rounded-lg bg-white p-6 shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md sm:p-8 md:mt-0">
-                <h2 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
-                    Change Password
-                </h2>
-                <form class="mt-4 space-y-4 md:space-y-5 lg:mt-5" action="#">
-                    <div>
-                        <label for="email" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Your email or phone</label>
-                        <input id="email" type="email" name="email" class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm" placeholder="name@company.com / 0911..." required="">
-                    </div>
-                    <div class="mr-2 flex flex-col">
-                        <label for="email" class="mb-1 block text-sm font-medium text-gray-900 dark:text-white">Send confirmation message via:  </label>
+    <div class="flex h-screen items-center justify-center">
+        <div class="w-full max-w-md">
+            <div v-if="showForgotPassword">
+                <div class="rounded bg-white px-8 py-6 shadow-md">
+                    <h2 class="mb-4 text-lg font-medium">Forgot Password</h2>
+                    <form>
+                        <div class="mb-4">
+                            <label class="mb-2 block font-bold text-gray-700" for="email">
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                class="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+                                placeholder="Enter your email address"
+                                type="email"
+                            />
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <button
+                                class="focus:shadow-outline rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 focus:outline-none"
+                                type="button"
+                                @click="forgotPassword"
+                            >
+                                Submit
+                            </button>
+                            <!--                            <button-->
+                            <!--                                class="inline-block align-baseline text-sm font-bold text-blue-500 hover:text-blue-800"-->
+                            <!--                                @click="showForgotPassword = false"-->
+                            <!--                            >-->
+                            <!--                                Cancel-->
+                            <!--                            </button>-->
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div v-if="showResetPassword">
+                <div class="rounded bg-white px-8 py-6 shadow-md">
+                    <h2 class="mb-4 text-lg font-medium">Reset Password</h2>
+                    <form>
+                        <div class="mb-4">
+                            <label class="mb-2 block font-bold text-gray-700" for="password">
+                                Password
+                            </label>
+                            <input
+                                class="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+                                placeholder="Enter your new password"
+                                type="password"
+                            />
+                        </div>
+                        <div class="mb-4">
+                            <label class="mb-2 block font-bold text-gray-700" for="password">
+                                Confirm Password
+                            </label>
+                            <input
+                                class="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+                                placeholder="confirm password"
+                                type="password"
+                            />
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <button
+                                class="focus:shadow-outline rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 focus:outline-none"
+                                type="button"
+                            >
+                                <a href="/login">
+                                    Reset Password
+                                </a>
 
-                        <div class="mb-4 flex items-center">
-                            <input id="default-radio-1" type="radio" value="" name="default-radio" class="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600">
-                            <label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">email</label>
+                            </button>
+                            <button
+                                class="inline-block align-baseline text-sm font-bold text-blue-500 hover:text-blue-800"
+                                @click="resetPassword"
+                            >
+                                Cancel
+                            </button>
                         </div>
-                        <div class="flex items-center">
-                            <input id="default-radio-2" checked type="radio" value="" name="default-radio" class="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600">
-                            <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">phone number</label>
-                        </div>
-
-                        <button class="mt-2 rounded border border-black py-1 px-4 text-black">
-                            Send
-                        </button>
-                    </div>
-                    <div>
-                        <input placeholder="otp" class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm" required="">
-                        <button class="mt-2 rounded border border-black py-1 px-4 text-black" @click="handleVerification">
-                            verify
-                        </button>
-                    </div>
-                    <div v-if="verification">
-                        <div>
-                            <label for="password" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">New Password</label>
-                            <input id="password" type="password" name="password" placeholder="••••••••" class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm" required="">
-                        </div>
-
-                        <div>
-                            <label for="confirm-password" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                            <input id="confirm-password" type="confirm-password" name="confirm-password" placeholder="••••••••" class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm" required="">
-                        </div>
-                        <button
-                            class="text-dark-50 my-4 mt-6 h-9 w-full rounded-md bg-gray-300 font-light"
-                        >
-                            Reset Password
-                        </button>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
 <script setup>
-import { ref } from 'vue';
 
-const verification = ref(false);
+import {ref} from 'vue';
 
-// Handle verification
-const handleVerification = () => {
-    verification.value = !verification.value;
+const showForgotPassword = ref(true);
+const showResetPassword = ref(false);
+
+function resetPassword() {
+    // logic for resetting password goes here
+    // once the password is reset, show the forgot password card again
+    showForgotPassword.value = true;
+    showResetPassword.value = false;
+}
+
+function forgotPassword() {
+    // logic for sending password reset email goes here
+    // once the email is sent, show the reset password card
+    showForgotPassword.value = false;
+    showResetPassword.value = true;
 }
 </script>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -5,27 +5,35 @@
         <div
             class="mx-2 mt-[5rem] flex h-fit w-full flex-col items-center py-4 sm:w-9/12 md:w-7/12 lg:w-5/12 xl:w-4/12"
         >
-            <div class="text-3xl font-light italic">
-                Rigel SMS
-            </div>
+            <!--            <div class="text-3xl font-light italic">-->
+            <!--                School Management System-->
+            <!--            </div>-->
+            <img alt="place holder" src="/assets/school.png"/>
             <div class="my-3 mb-6 text-xs font-light">
-                welcome back, please login to your account
+                welcome back, please login to your account.
             </div>
 
             <!--        email or phone input section-->
             <div class="mt-4 w-11/12">
                 <div class="flex items-center justify-center">
+                    <div
+                        class="bg-dark-50 dark:bg-dark-100 flex h-9 items-center rounded-l-md px-2"
+                    >
+                        <svg
+                            class="h-6 w-6" fill="none" stroke="currentColor" stroke-width="1.5"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        </svg>
+
+                    </div>
                     <input
                         v-model="form.emailOrPhone"
                         class="h-8 w-full rounded-md pl-2 text-[0.6rem] font-light"
                         placeholder="Email or Phone"
                         type="text"/>
-                </div>
-                <div
-
-                    class="text-dark-50 mt-1 ml-1 text-[0.55rem]"
-                >
-                    Email or Phone
                 </div>
 
             </div>
@@ -33,17 +41,25 @@
             <!--      Password input section-->
             <div class="mt-4 w-11/12">
                 <div class="flex items-center justify-center">
+                    <div
+                        class="bg-dark-50 dark:bg-dark-100 flex h-9 items-center rounded-l-md px-2"
+                    >
+                        <svg
+                            class="h-6 w-6" fill="none" stroke="currentColor" stroke-width="1.5"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        </svg>
+                    </div>
                     <input
                         v-model="form.password"
                         class="h-8 w-full rounded-md pl-2 text-[0.6rem] font-light"
                         placeholder="Password"
                         type="password"/>
                 </div>
-                <div
-                    class="text-dark-50 ml-1 text-[0.55rem]"
-                >
-                    Password
-                </div>
+
             </div>
 
             <!--      Remember me and forgot password section-->
@@ -84,6 +100,9 @@
                     Sign up
                 </a>
             </div>
+        </div>
+        <div class="lg:my-18 mb-6 md:mb-0 md:w-8/12 lg:mr-5 lg:w-5/12">
+            <img alt="place holder" class="ml-36 w-8/12" src="/assets/Allura.png    "/>
         </div>
     </div>
 </template>
