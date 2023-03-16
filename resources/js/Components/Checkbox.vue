34df9a06c7d6acbd3525e4e178ce7ddde6d67ede
adilabdu <adilabdu68@gmail.com>
2023-03-16 13:19:39 +0300

    Define new web components `Table` `Checkbox` and `TertiaryButton`

---
+++ resources/js/Components/Checkbox.vue
@@ -0,0 +1,27 @@
+<template>
+    <input
+        :checked="modelValue"
+        class="h-3.5 w-3.5 rounded-sm"
+        type="checkbox" @input="input"
+    >
+</template>
+
+<script setup>
+defineProps({
+    modelValue: {
+        type: Boolean,
+        required: true
+    }
+})
+
+const emits = defineEmits(['update:modelValue'])
+
+function input(input) {
+    emits('update:modelValue', input.target.checked)
+}
+
+</script>
+
+<style scoped>
+
+</style>
