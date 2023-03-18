065f6e7f1215533b36856f0001a7092cf1f9a274
adilabdu <adilabdu68@gmail.com>
2023-03-18 22:01:02 +0300

    Create new `Card` component
    - `StatisticsCard` view component implementable for statistics
    - `UsersStatistics` serves as example on how to call the view

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,65 @@
+<template>
+
+    <div
+        class="flex w-full flex-col justify-between gap-3 rounded-md border bg-white p-4 shadow-sm md:w-1/2 md:max-w-xl">
+
+        <div
+            v-if="header"
+            :class="[ header && !!! title || header && !!! subtitle ? 'items-center' : 'items-start' ]"
+            class="flex justify-between"
+        >
+
+            <div class="flex flex-col">
+                <h3 class="font-semibold">
+                    <slot name="title">
+                        {{ title }}
+                    </slot>
+                </h3>
+                <h3 class="text-sm text-gray-500">
+                    <slot name="subtitle">
+                        {{ subtitle }}
+                    </slot>
+                </h3>
+            </div>
+
+            <div v-if="icon" class="stroke-1.5 my-0.5 h-6 w-6">
+                <slot name="icon">
+                    <UsersIcon/>
+                </slot>
+            </div>
+
+        </div>
+
+        <slot/>
+
+    </div>
+
+</template>
+
+<script setup>
+import {computed} from "vue"
+import {UsersIcon} from "@heroicons/vue/24/outline"
+
+const header = computed(() => {
+    return (!!props.title || !!props.subtitle)
+})
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: 'Title of the card'
+    },
+    subtitle: {
+        type: String,
+        default: 'Subtitle of the card with detailed explanation'
+    },
+    icon: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Views/StatisticsCard.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <Card
+        :subtitle="subtitle"
+        :title="title" class="md:col-span-1 md:row-span-1 md:w-full"
+        icon
+    >
+        <h3 class="flex flex-col">
+            <slot>
+                <span class="text-2xl font-semibold">
+                    {{ data }}
+                </span>
+            </slot>
+        </h3>
+        <template #icon>
+            <slot name="icon"/>
+        </template>
+    </Card>
+
+</template>
+
+<script setup>
+import Card from "@/Components/Card.vue"
+
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    subtitle: {
+        type: String,
+        required: true,
+    },
+    data: {
+        type: String,
+        default: null
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
