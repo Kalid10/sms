2cb7732e9837efbd7115bcc9a78a2044dce1671d
adilabdu <adilabdu68@gmail.com>
2023-03-21 17:49:55 +0300

    Introduce new `Cell` components to render inside table row

---
+++ resources/js/Components/LinkCell.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <Link :href="href">
+        {{ value }}
+    </Link>
+
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3'
+
+defineProps({
+    value: {
+        type: String,
+        required: true
+    },
+    href: {
+        type: String,
+        required: true
+    }
+})
+
+</script>
+
+<style scoped>
+
+</style>
