2d607521ac19daf7faaf5b5cfaca0abae4e6b2b1
Biniyam Lemma <lemmasbini@gmail.com>
2023-03-23 22:06:21 +0300

    -Handle file upload and emit the file in the `FileUpload` component and import it `Student` registration page.

--- resources/js/Components/FileInput.vue
+++ resources/js/Components/FileInput.vue
@@ -2,15 +2,15 @@
     <div class="flex h-4/5 w-full items-center justify-center">
         <label
             class="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-100"
-            :class="{'bg-gray-200': isDragging}"
+            :class="{ 'bg-gray-200': isDragging }"
             @dragover.prevent="isDragging = true"
             @dragleave.prevent="isDragging = false"
             @drop="handleDrop"
-
         >
             <div class="flex flex-col items-center justify-center pt-5 pb-6">
                 <CloudArrowUpIcon class="h-10 w-10"/>
-                <p class="mb-2 text-sm text-gray-500"><span class="font-bold">Click to upload</span> or drag and drop
+                <p class="mb-2 text-sm text-gray-500">
+                    <span class="font-bold">Click to upload</span> or drag and drop
                 </p>
                 <p class="text-xs text-gray-500">(MAX. 800x400px)</p>
             </div>
@@ -25,8 +25,7 @@
             </div>
             <div v-if="show" class="my-2">
                 <snap v-model="form.file" class="text-sm font-light">
-                    file:
-                    {{ form.file.name }}
+                    file: {{ form.file.name }}
                 </snap>
             </div>
             <div v-if="show">
@@ -39,40 +38,40 @@
 </template>
 
 <script setup>
-import {computed, reactive, ref} from 'vue'
-import {CloudArrowUpIcon} from '@heroicons/vue/24/outline'
+import {computed, reactive, ref} from "vue";
+import {CloudArrowUpIcon} from "@heroicons/vue/24/outline";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 
 const form = reactive({
-    file: null,
-})
+    file: null
+});
 
 const isDragging = ref(false);
 
-const show = computed(() => form.file !== null)
+const show = computed(() => form.file !== null);
 
-const handleFileUpload = (event) => {
-    const files = event.target.files
+const handleFileUpload = event => {
+    const files = event.target.files;
     if (files.length > 0) {
-        form.file = files[0]
+        form.file = files[0];
     }
-}
+};
+
+const emits = ['file-uploaded'];
 
 const handleUpload = () => {
-    // Add code to upload the file here
-    console.log('Uploading file:', form.file)
-}
+    emit('file-uploaded', form.file);
+};
 
-const handleDrop = (event) => {
-    event.preventDefault()
-    const files = event.dataTransfer.files
+const handleDrop = event => {
+    event.preventDefault();
+    const files = event.dataTransfer.files;
     if (files.length > 0) {
-        form.file = files[0]
+        form.file = files[0];
     }
-}
+};
 
-const fileInput = ref(null)
-
+const fileInput = ref(null);
 </script>
 
 <style scoped>
--- resources/js/Pages/Users/Create/Student.vue
+++ resources/js/Pages/Users/Create/Student.vue
@@ -9,10 +9,17 @@
     </StudentFormElement>
 
     <StudentFormElement title="Register students by mass" subtitle="only upload an excel or csv file">
-        <StudentFileInput label="Upload Excel File" placeholder="Upload an excel file"/>
+        <StudentFileInput @file-uploaded="onFileUploaded"/>
     </StudentFormElement>
 </template>
 <script setup>
 import StudentFormElement from "@/Components/FormElement.vue"
 import StudentTextInput from "@/Components/TextInput.vue"
-import StudentFileInput from "@/Components/FileInput.vue";</script>
+import StudentFileInput from "@/Components/FileInput.vue"
+
+defineEmits(['file-uploaded']);
+
+const onFileUploaded = (file) => {
+    console.log("File uploaded:", file);
+};
+</script>
