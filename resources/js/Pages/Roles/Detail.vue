4b9b1e0a429fa74a11957d3623f59d03e23dbfdb
yosef-seboka <yosefseboka2006@gmail.com>
2023-03-23 07:28:25 +0300

    Edit assign role

--- resources/js/Pages/Roles/Detail.vue
+++ resources/js/Pages/Roles/Detail.vue
@@ -1,24 +1,24 @@
 <template>
-    <div class="flex flex-col space-x-2 p-4 md:flex-row">
-<!--    assigned rols -->
-    <Card title="Assigned roles:" subtitle="" class="mb-2 md:w-3/5">
+    <div class="flex w-full flex-col space-x-2 bg-green-300 p-4 md:flex-row">
+
+<!--    Assigned rols -->
+    <Card title="Assigned roles:" subtitle="" class="mb-2 border border-black md:w-3/5 ">
         <div class="">
-            <div class="hover:~bg-red-500/10">
-            <div>
+            <div class="border border-pink-400">
+            <div class="">
                 <ul>
-                    <li v-for="role in user_roles" :key="role.id" class="m-2 flex flex-row justify-between p-1 hover:bg-gray-100">
+                    <li v-for="role in newUserRoles" :key="role.id" class="m-2 flex flex-row justify-between p-1 hover:bg-gray-100">
                        <div>
-                           <p :class="deleted">{{role.name}}</p>
+                           <p :class="[deleted, { 'line-through': role.deleted }]">{{role.name}}</p>
                        </div>
                         <div>
-                            <TertiaryButton title="Remove"></TertiaryButton>
+                            <TertiaryButton title="Remove" @click="deleteRole(role)"></TertiaryButton>
                         </div>
                     </li>
-
                 </ul>
             </div>
                 <div class="flex justify-end">
-                    <PrimaryButton title="Submit" class="m-4"></PrimaryButton>
+                    <PrimaryButton title="Update" class="m-4" @click="showDialog=true"></PrimaryButton>
                 </div>
             </div>
         </div>
@@ -27,43 +27,73 @@
 
 
 
-
-<!--    assign new role -->
-    <div class=" mt-2 h-fit border border-blue-400 md:w-2/5 ">
-        <div class=" ">
-            <PrimaryButton class="inline-flex items-center justify-between px-4 py-3 text-center text-sm font-medium text-white"  type="button" @click="showRolesDropdown=!showRolesDropdown">Assign new Role
-                <svg class="ml-2 h-5 w-5" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
-            </PrimaryButton>
-
-
-            <!-- Dropdown menu -->
-            <div v-if="showRolesDropdown" class="z-10 w-full bg-white">
-                <div class="p-3">
-                        <SearchRoleTextInput type="text" class="block w-full text-sm text-gray-900 " placeholder="Search roles"></SearchRoleTextInput>
+        <!-- list of roles   -->
+        <div class="border border-red-400 md:p-2 lg:w-full">
+            <div class="mx-auto my-10 max-w-2xl rounded-xl bg-white p-8 shadow shadow-slate-300">
+                <div class="flex flex-row items-center justify-between space-x-reverse">
+                    <h1 class="text-2xl font-semibold text-slate-700">Roles</h1>
                 </div>
-                <ul class="h-48 overflow-y-auto px-3 pb-3 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownSearchButton">
-                    <li>
-                        <div class="flex items-center rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-                            <input id="checkbox-item-11" type="checkbox" value="" class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700">
-                            <label for="checkbox-item-11" class="ml-2 w-full rounded text-sm font-medium text-gray-900 dark:text-gray-300">Admin</label>
+                <p class="text-slate-500">List of roles available</p>
+                <p>{{ updatedRoles }}</p>
+                <SearchRoleTextInput placeholder="Search role"></SearchRoleTextInput>
+
+                <div v-for="role in user_roles" :key="role.id" class="my-3 ">
+                    <div class="flex items-center justify-between border-b border-l-4 border-slate-200 border-l-transparent bg-gradient-to-r  from-transparent to-transparent py-3 px-2 transition duration-150 ease-linear hover:from-slate-100">
+                        <div class="inline-flex items-center space-x-2">
+                            <div>
+                                <input v-model="newUserRoles" type="checkbox" :value="role.name" class="h-5 w-5 text-blue-600 transition duration-150 ease-in-out">
+                            </div>
+                            <div>{{ role.name }}</div>
                         </div>
-                    </li>
-                    <li>
-                        <div class="flex items-center rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-                            <input id="checkbox-item-12" checked type="checkbox" value="" class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700">
-                            <label for="checkbox-item-12" class="ml-2 w-full rounded text-sm font-medium text-gray-900 dark:text-gray-300">Manage Teachers</label>
-                        </div>
-                    </li>
-                </ul>
-                <div class="flex justify-end">
-                    <PrimaryButton title="Add" class="m-4"></PrimaryButton>
+
+                    </div>
+
                 </div>
             </div>
         </div>
+
+
+<!--    assign new role -->
+    <div class=" mt-2 h-fit  md:w-2/5 ">
+<!--        <div class="">-->
+<!--            <PrimaryButton class="inline-flex items-center justify-between px-4 py-3 text-center text-sm font-medium text-white"  type="button" @click="showRolesDropdown=!showRolesDropdown">Assign new Role-->
+<!--                <svg class="ml-2 h-5 w-5" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>-->
+<!--            </PrimaryButton>-->
+
+
+<!--            &lt;!&ndash; Dropdown menu &ndash;&gt;-->
+<!--            <div v-if="showRolesDropdown" class="z-10 w-full bg-white">-->
+<!--                <div class="p-3">-->
+<!--                        <SearchRoleTextInput type="text" class="block w-full text-sm text-gray-900 " placeholder="Search roles"></SearchRoleTextInput>-->
+<!--                </div>-->
+<!--                <ul class="h-48 overflow-y-auto px-3 pb-3 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownSearchButton">-->
+<!--                    <li>-->
+<!--                        <div class="flex items-center rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">-->
+<!--                            <input id="checkbox-item-11" type="checkbox" value="" class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700">-->
+<!--                            <label for="checkbox-item-11" class="ml-2 w-full rounded text-sm font-medium text-gray-900 dark:text-gray-300">Admin</label>-->
+<!--                        </div>-->
+<!--                    </li>-->
+<!--                    <li>-->
+<!--                        <div class="flex items-center rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">-->
+<!--                            <input id="checkbox-item-12" checked type="checkbox" value="" class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700">-->
+<!--                            <label for="checkbox-item-12" class="ml-2 w-full rounded text-sm font-medium text-gray-900 dark:text-gray-300">Manage Teachers</label>-->
+<!--                        </div>-->
+<!--                    </li>-->
+<!--                </ul>-->
+<!--                <div class="flex justify-end">-->
+<!--                    <PrimaryButton title="Add" class="m-4" @click="showDialog = true"></PrimaryButton>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+        </div>
     </div>
 
-    </div>
 
+    <DialogBox
+        v-if="showDialog"
+        @confirm="handleConfirm"
+        @close="showDialog= false">
+    </DialogBox>
 
 
     <p>{{activities}} activity logs </p>
@@ -74,7 +104,7 @@
     <div class="rounded-xl bg-white p-8 md:pt-14 ">
         <h3 class="pb-5 text-xl font-bold">Logs</h3>
         <ul class="flex max-w-2xl flex-col divide-y divide-gray-200 dark:divide-gray-700">
-            <li  v-for="activityLog in activityLogs" :key="activityLog.date" class="justify-center pb-3 sm:pb-4">
+            <li  v-for="activityLog in activities" :key="activityLog.date" class="justify-center pb-3 sm:pb-4">
                 <div class="flex items-center space-x-5 p-1">
                     <div class="shrink-0">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
@@ -98,6 +128,11 @@
     </div>
 </template>
 
+
+
+
+
+
 <script setup>
 import {computed, onMounted,ref} from "vue";
 import {router, usePage} from "@inertiajs/vue3";
@@ -105,39 +140,78 @@ import {router, usePage} from "@inertiajs/vue3";
 import PrimaryButton from "@/Components/PrimaryButton.vue"
 import TertiaryButton from "@/Components/TertiaryButton.vue";
 import SearchRoleTextInput from "@/Components/TextInput.vue"
-import TableElement from "@/Components/TableElement.vue";
-import RolesGroupedCheckBox from "@/Components/GroupedCheckBox.vue"
+import DialogBox from "@/Components/DialogBox.vue";
 import Card from "@/Components/Card.vue"
+import {now} from "@vueuse/core";
 
-const showRolesDropdown =  ref(false)
+const showDialog = ref(false)
+const updatedRoles = ref([])
+function handleConfirm(confirmationType) {
+    assignRoles();
+    showDialog.value = false
+
+}
+
 
 const user_roles = computed(() => {
     return usePage().props.user_roles;
 });
 
-// onMounted(() =>{
-//     roleActivities();
-// })
+const all_roles = computed(() =>{
+    return usePage().props.roles;
+})
 
+
+const newUserRoles = ref([...user_roles.value]);
+// alert(newUserRoles.value);
+
+    function deleteRole(role){
+        const index = newUserRoles.value.indexOf(role);
+        if (index > -1) {
+            newUserRoles.value.splice(index, 1);
+        }
+        role.deleted_at = now();
+        updatedRoles.value.push(role);
+
+    }
+
+
+onMounted(() =>{
+    // roleActivities();
+})
 
 const activities = computed(()=>{
     return usePage().props.activities;
 })
 
-// function roleActivities() {
-//     alert("Role activity")
-//     router.get('/roles/activities', {
-//         roles: ["manage-roles"],
-//         user_id: 1
-//     }, {
-//         preserveState: true,
-//         onSuccess: () =>{
-//             console.log("Success")
-//         },
-//         onError: (error) =>{
-//             console.log("Error")
-//             console.log(error)
-//         }
-//     })
-// }
+function roleActivities(row) {
+    alert("Role activity")
+    router.get('/roles/activities', {
+        roles: ["manage-roles"],
+        user_id:1
+    }, {
+        preserveState: true,
+        onSuccess: () =>{
+            console.log("Success")
+        },
+        onError: (error) =>{
+            console.log("Error")
+            console.log(error)
+        }
+    })
+}
+function assignRoles() {
+    router.post('/roles/assign', {
+        user_id: 2,
+        roles: ["manage-roles", "manage-subjects"]
+    }, {
+        onSuccess: () =>{
+            console.log("Success")
+        },
+        onError: (error) =>{
+            console.log("Error")
+            console.log(error)
+        }
+    })
+}
 </script>
