1c352cf32763cbb3c6c6a888740572182d5dcfb7
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-03-23 23:58:32 +0300

    Add batch seeder and update batch factory

--- database/factories/BatchFactory.php
+++ database/factories/BatchFactory.php
@@ -5,8 +5,8 @@ namespace Database\Factories;
 use App\Models\Batch;
 use App\Models\Level;
 use App\Models\SchoolYear;
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Log;
 
 class BatchFactory extends Factory
 {
@@ -24,13 +24,15 @@ class BatchFactory extends Factory
      */
     public function definition()
     {
+        $level = Level::inRandomOrder()->first() ?? Level::factory()->create();
+        $schoolYear = SchoolYear::whereNull('end_date')->first() ?? SchoolYear::factory()->create();
+
+        Log::info(SchoolYear::whereNull('end_date')->first());
+
         return [
-            'level_id' => Level::all()->random()->id,
-            'school_year_id' => SchoolYear::create([
-                'start_date' => Carbon::now()->subYear(),
-                'end_date' => null,
-            ])->id,
-            'section' => $this->faker->unique()->randomLetter,
+            'level_id' => $level->id,
+            'school_year_id' => $schoolYear->id,
+            'section' => $this->faker->regexify('[A-Z]{1}'),
         ];
     }
 }
