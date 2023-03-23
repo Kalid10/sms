0ef43834d159f6e61f39b6202a17541c6251e742
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-03-23 23:10:03 +0300

    Add GET homeroom teachers endpoint

--- routes/web/teachers.php
+++ routes/web/teachers.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('teachers/')->middleware(['checkUserRole:manage-teachers'])->name('teachers.')->group(function () {
     Route::post('assign/homeroom', [TeacherController::class, 'assignHomeroomTeacher'])->name('assign.homeroom');
     Route::delete('remove/homeroom/{id}', [TeacherController::class, 'removeHomeroomTeacher'])->name('remove.homeroom');
+    Route::get('homerooms', [TeacherController::class, 'getHomeroomTeachers'])->name('homeroom');
 });
