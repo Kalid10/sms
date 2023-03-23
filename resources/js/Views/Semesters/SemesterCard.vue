9a43005a8d7d189249877431a17852dab64efa70
adilabdu <adilabdu68@gmail.com>
2023-03-23 10:27:53 +0300

    Build view components for semesters page

---
+++ resources/js/Views/Semesters/SemesterCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <Card :icon="semester.status === 'active'" :title="semester.name" :subtitle="semester.school_year" class="!w-full !max-w-full">
+        <div class="flex items-start justify-between">
+
+            <div class="flex gap-4">
+                <div class="flex items-center gap-1">
+                    <div
+                        :class="{
+                                'animate-pulse bg-positive-50': semester.status === 'active',
+                                'bg-blue-400': semester.status === 'upcoming',
+                                'bg-neutral-200': semester.status === 'completed'
+                            }"
+                        class="h-3 w-3 rounded-full"
+                    />
+                    <span class="text-xs capitalize">{{ semester.status }}</span>
+                </div>
+                <span class="text-xs">Updated {{ moment(semester.last_updated).fromNow() }}</span>
+            </div>
+
+        </div>
+
+        <template #icon>
+            <CircularProgress
+                v-if="semester.status === 'active'"
+                :dasharray="75"
+                :diameter="30"
+                :percentage="74"
+                color="positive"
+            />
+        </template>
+
+    </Card>
+</template>
+
+<script setup>
+import moment from "moment";
+import Card from "@/Components/Card.vue";
+import CircularProgress from "@/Components/CircularProgress.vue";
+
+defineProps({
+    semester: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Views/Semesters/SemestersByStatus.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div class="container mx-auto flex w-full flex-col items-center gap-3">
+        <Heading>{{ status }} Semesters</Heading>
+        <SemestersList :semesters="semesters.filter(semester => semester.status === status).slice().reverse()" />
+    </div>
+
+</template>
+
+<script setup>
+import {semesters} from "@/fake";
+import Heading from "@/Components/Heading.vue";
+import SemestersList from "@/Views/Semesters/SemestersList.vue";
+
+defineProps({
+    status: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
