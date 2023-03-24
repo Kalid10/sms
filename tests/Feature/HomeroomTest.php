9de578ee268cd1936c49a3822c34bea95c884525
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-03-24 12:37:52 +0300

    Add GET homerooms test

--- tests/Feature/HomeroomTest.php
+++ tests/Feature/HomeroomTest.php
@@ -4,9 +4,11 @@ use App\Models\Batch;
 use App\Models\HomeroomTeacher;
 use App\Models\Role;
 use App\Models\Teacher;
+use Database\Seeders\HomeroomTeacherSeeder;
 use Database\Seeders\SchoolYearSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Artisan;
+use Inertia\Testing\AssertableInertia;
 
 uses(RefreshDatabase::class);
 
@@ -25,7 +27,7 @@ beforeEach(function () {
     $this->batch = Batch::factory()->create();
 });
 
-test('assignHomeroomTeacher method works correctly', function () {
+it('correctly assigns a homeroom teacher', function () {
     $response = $this->actingAs($this->teacher->user)
         ->post(route('teachers.assign.homeroom'), [
             'teacher_id' => $this->teacher->id,
@@ -38,7 +40,7 @@ test('assignHomeroomTeacher method works correctly', function () {
     expect(HomeroomTeacher::where('teacher_id', $this->teacher->id)->where('batch_id', $this->batch->id)->exists())->toBeTrue();
 });
 
-test('removeHomeroomTeacher method works correctly', function () {
+it('correctly removes a homeroom teacher', function () {
     $homeroomTeacher = HomeroomTeacher::create([
         'teacher_id' => $this->teacher->id,
         'batch_id' => $this->batch->id,
@@ -50,4 +52,49 @@ test('removeHomeroomTeacher method works correctly', function () {
     $response->assertRedirect();
     $response->assertSessionHas('success', 'Homeroom teacher removed successfully.');
     expect(HomeroomTeacher::find($homeroomTeacher->id))->toBeNull();
+});
+
+it('returns all homeroom teachers with pagination when no ids are provided', function () {
+    // Seed homeroom teachers
+    $this->seed(HomeroomTeacherSeeder::class);
+
+    // Send a request to the endpoint
+    $response = $this->actingAs($this->teacher->user)->get('/teachers/homerooms');
+
+    // Assertions
+    $response->assertStatus(200)
+        ->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('Welcome')
+//            ->has('homerooms.data', 1)
+        );
+});
+
+it('returns the homeroom teacher for a given teacher id', function () {
+    // Create test data
+    HomeroomTeacher::factory()->create(['teacher_id' => $this->teacher->id]);
+
+    $response = $this->actingAs($this->teacher->user)->get('/teachers/homerooms?teacher_id='.$this->teacher->id);
+
+    // Assertions
+    $response->assertStatus(200)
+        ->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('Welcome')
+        );
+});
+
+it('returns the homeroom teacher for a given batch id', function () {
+    // Create test data
+    $batch = Batch::factory()->create();
+
+    HomeroomTeacher::factory()->create(['batch_id' => $batch->id]);
+
+    // Send a request to the endpoint
+    $response = $this->actingAs($this->teacher->user)->get('/teachers/homerooms/?batch_id='.$batch->id);
+
+    // Assertions
+    // TODO: Change the component name to the correct one
+    $response->assertStatus(200)
+        ->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('Welcome')
+        );
 });
