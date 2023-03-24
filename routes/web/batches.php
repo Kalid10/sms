a03d530b674cf69299e9fee2e847d735a524f038
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-03-24 13:52:28 +0300

    Group routes with same controllers

--- routes/web/batches.php
+++ routes/web/batches.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\BatchController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('batches/')->middleware(['checkUserRole:manage-batches'])->name('batches.')->group(function () {
-    Route::post('create', [BatchController::class, 'create'])->name('create');
-    Route::post('create_bulk', [BatchController::class, 'createBulk'])->name('create');
-    Route::get('', [BatchController::class, 'list'])->name('list');
-    Route::get('active', [BatchController::class, 'active'])->name('active');
+Route::controller(BatchController::class)->prefix('batches/')->middleware(['checkUserRole:manage-batches'])->name('batches.')->group(function () {
+    Route::post('create', 'create')->name('create');
+    Route::post('create_bulk', 'createBulk')->name('create');
+    Route::get('', 'list')->name('list');
+    Route::get('active', 'active')->name('active');
 });
--- routes/web/semesters.php
+++ routes/web/semesters.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\SemesterController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('semesters/')->middleware(['checkUserRole:manage-semesters'])->name('semesters.')->group(function () {
-    Route::get('', [SemesterController::class, 'index'])->name('index');
+Route::controller(SemesterController::class)->prefix('semesters/')->middleware(['checkUserRole:manage-semesters'])->name('semesters.')->group(function () {
+    Route::get('', 'index')->name('index');
     Route::post('create', [SemesterController::class, 'create'])->name('create');
     Route::post('update', [SemesterController::class, 'update'])->name('update');
     Route::get('list', [SemesterController::class, 'list'])->name('list');
