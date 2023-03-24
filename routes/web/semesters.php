4762f09c0e4c522a94bd6c3c7461a6cd9d845595
adilabdu <adilabdu68@gmail.com>
2023-03-24 16:55:37 +0300

    Implement `/semesters/{id}` page

--- routes/web/semesters.php
+++ routes/web/semesters.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(SemesterController::class)->prefix('semesters/')->middleware(['checkUserRole:manage-semesters'])->name('semesters.')->group(function () {
     Route::get('', 'index')->name('index');
+    Route::get('{id}', 'show')->name('show');
     Route::post('create', [SemesterController::class, 'create'])->name('create');
     Route::post('update', [SemesterController::class, 'update'])->name('update');
     Route::get('list', [SemesterController::class, 'list'])->name('list');
