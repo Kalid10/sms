2aab572fb73cc466ff27f21feefd839946f999f3
adilabdu <adilabdu68@gmail.com>
2023-03-26 21:53:31 +0300

    Reset font size for title and subtitle

--- resources/js/Components/FormElement.vue
+++ resources/js/Components/FormElement.vue
@@ -6,9 +6,9 @@
         class="flex min-h-10 w-full flex-col border bg-white">
 
         <div class="flex flex-col gap-3 p-4">
-            <div class="flex flex-col text-sm">
+            <div class="flex flex-col">
                 <h3>{{ title }}</h3>
-                <h5 class="text-xs text-gray-500">
+                <h5 class="text-sm text-gray-500">
                     {{ subtitle }}
                 </h5>
             </div>
