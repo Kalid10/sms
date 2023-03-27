c6a1c5b9e1609a97f3a662cd23c56f114ce3112b
yosef-seboka <yosefseboka2006@gmail.com>
2023-03-27 09:42:39 +0300

    Add test routes for admin,year,year registration

--- routes/web/users.php
+++ routes/web/users.php
@@ -6,3 +6,14 @@ use Inertia\Inertia;
 Route::get('/users', function () {
     return Inertia::render('Users/Index');
 });
+
+// UI test routes
+Route::get('/registration', function () {
+    return Inertia::render('Users/UserRegistration');
+});
+Route::get('/year-registration', function () {
+    return Inertia::render('Users/SchoolYearRegistration');
+});
+Route::get('/batch-registration', function () {
+    return Inertia::render('Users/BatchRegistration');
+});
