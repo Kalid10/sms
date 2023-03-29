92660019e50024e90ae492754e65632e5b0bd328
yosef-seboka <yosefseboka2006@gmail.com>
2023-03-29 17:53:49 +0300

    Remove the color, fix the typo, and make modelValue required

--- resources/js/Components/Toggle.vue
+++ resources/js/Components/Toggle.vue
@@ -7,7 +7,7 @@
                 class="peer sr-only"
                 @input="input"
             >
-            <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-green-800"></div>
+            <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-black peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-black dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-black"></div>
         </label>
     </div>
 </template>
@@ -15,9 +15,9 @@
 defineProps({
     modelValue: {
         type: Boolean,
-        default: false,
+        required: true,
     },
-    lable:{
+    label:{
         type:String,
         default: null
     }
