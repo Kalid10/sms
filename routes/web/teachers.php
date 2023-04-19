ed870401e9e23943af3b8d565671a90cbec9376e
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-04-20 02:24:08 +0300

    Add teachers list page at /teachers with optimized data fetching

--- routes/web/teachers.php
+++ routes/web/teachers.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(TeacherController::class)->prefix('teachers/')->middleware(['checkUserRole:manage-teachers'])->name('teachers.')->group(function () {
+    Route::get('/', 'index')->name('index');
     Route::post('assign/homeroom', 'assignHomeroomTeacher')->name('assign.homeroom');
     Route::delete('remove/homeroom/{id}', 'removeHomeroomTeacher')->name('remove.homeroom');
     Route::get('homerooms', 'getHomeroomTeachers')->name('homeroom');
