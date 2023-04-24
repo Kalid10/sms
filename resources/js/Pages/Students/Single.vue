1d99ee3614ca13166d936a4b0e42a4842ffdda22
adilabdu <adilabdu68@gmail.com>
2023-04-24 17:13:02 +0300

    WIP: Students Profile Page update, Schedule viewer

--- resources/js/Pages/Students/Single.vue
+++ resources/js/Pages/Students/Single.vue
@@ -17,7 +17,7 @@
                     <div class="mx-auto flex w-fit flex-col items-center">
                         <Heading class="!font-semibold" size="lg">{{ student.user.name }}</Heading>
                         <div class="flex w-full min-w-[28px] justify-start gap-2 leading-none">
-                            <h3 class="text-gray-500" size="">12B</h3>
+<!--                            <h3 class="text-gray-500" size="">12B</h3>-->
                         </div>
                     </div>
 
@@ -28,33 +28,99 @@
 
         </div>
 
-        <div class="col-span-12 h-full overflow-auto p-2 md:col-span-10">
+        <div class="col-span-12 flex h-full flex-col gap-6 overflow-auto p-2 md:col-span-10">
 
-            <div class="flex flex-col gap-4">
+            <StudentOverview />
 
-                <TabElement :tabs="tabs">
+            <div class="flex w-full gap-4">
+                <Card class="!max-w-xs" :title="`Notes about ${student.user.name.split(' ').slice(-1)}`" icon>
 
-                    <template #personal_details>
+                    <ul class="flex flex-col gap-4">
+                        <li v-for="(note, n) in notes" :key="n">
+                            <h3 class="text-sm">{{ note.title }}</h3>
+                            <h3 class="note-preview text-sm text-gray-500">
+                                {{ note.body }}
+                            </h3>
+                            <div class="mt-1 flex w-full gap-2 text-xs">
+                                <div class="flex items-center gap-1">
+                                    <div :class="note.party === 'parent' ? 'bg-yellow-600' : 'bg-green-600'" class="h-2.5 w-2.5 rounded-full"/>
+                                    <h3 class="">{{ note.author }}</h3>
+                                </div>
+                                <h3 class="italic text-gray-500">{{ note.date }}</h3>
+                            </div>
+                        </li>
+                    </ul>
 
-                        <StudentPersonalDetail/>
+                    <TertiaryButton>View All</TertiaryButton>
 
-                    </template>
+                </Card>
 
-                    <template #guardian>
+                <div class="flex grow flex-col justify-between gap-3">
 
-                        <StudentGuardian/>
+                    <h3 class="text-sm font-semibold">Class Schedule</h3>
 
-                    </template>
+                    <div class="flex h-full w-full rounded-md border">
 
-                    <template #notes>
+                        <div class="hours grid grid-cols-1 flex-col">
 
-                        <NotesAboutStudent/>
+                            <div class="border-b"></div>
+                            <div v-for="(period, p) in periods" :key="p" class="grid place-items-center border-r border-b px-2 text-xs font-semibold text-gray-500 last:border-b-0">{{ period['start_time'] }}</div>
 
-                    </template>
+                        </div>
 
-                </TabElement>
+                        <div class="days grid w-full">
+                            <div class="row-span-1 grid h-full w-full grid-cols-5 grid-rows-1">
+                                <div
+                                    v-for="(day, d) in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']"
+                                    :key="d"
+                                    class="grid place-items-center border-b border-r text-xs font-semibold text-gray-500 last:border-r-0"
+                                >
+                                    {{ day }}
+                                </div>
+                            </div>
+                            <div class="subjects row-span-auto w-full">
+
+                                <div class="sessions grid h-full grow grid-flow-col-dense grid-cols-5">
+
+                                    <template v-for="(schedule, s) in schedules" :key="s">
+                                        <div
+                                            v-if="! (schedule['day_of_week'] !== 'monday' && schedule['batch_subject'] === null)"
+                                            :class="schedule['school_period']['is_custom'] ?
+                                                'bg-brand-50/15 text-brand-100 col-span-5 border-r-0' :
+                                                'col-span-1 session'"
+                                            class="row-span-1 flex items-center justify-center border-r border-b text-sm font-semibold"
+                                        >
+                                            {{ getPeriodName(schedule) }}
+                                        </div>
+                                    </template>
+
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+<!--                    <div class="sessions grid grow grid-flow-col-dense grid-cols-5 rounded-md border">-->
+
+<!--                        <template v-for="(schedule, s) in schedules" :key="s">-->
+<!--                            <div-->
+<!--                                v-if="! (schedule['day_of_week'] !== 'monday' && schedule['batch_subject'] === null)"-->
+<!--                                :class="schedule['school_period']['is_custom'] ?-->
+<!--                                    'bg-brand-50/15 text-brand-100 col-span-5 border-r-0' :-->
+<!--                                    'col-span-1 session'"-->
+<!--                                class="row-span-1 flex items-center justify-center border-r border-b text-sm font-semibold"-->
+<!--                            >-->
+<!--                                {{ getPeriodName(schedule) }}-->
+<!--                            </div>-->
+<!--                        </template>-->
+
+<!--                    </div>-->
+
+                </div>
 
             </div>
+
 
         </div>
 
@@ -64,20 +130,53 @@
 
 <script setup>
 import {computed} from "vue"
+import {notes} from "@/fake.js"
 import {usePage} from "@inertiajs/vue3";
 import Heading from "@/Components/Heading.vue";
-import TabElement from "@/Components/TabElement.vue";
-import StudentPersonalDetail from "@/Views/Students/StudentPersonalDetail.vue"
-import StudentGuardian from "@/Views/Students/StudentGuardian.vue"
-import NotesAboutStudent from "@/Views/Students/NotesAboutStudent.vue";
+import Card from "@/Components/Card.vue"
+import StudentOverview from "@/Views/Students/StudentOverview.vue"
+import TertiaryButton from "@/Components/TertiaryButton.vue";
 
 const student = computed(() => usePage().props.student)
+const schedules = computed(() => usePage().props.schedule)
+const periods = computed(() => usePage().props.periods)
+const periodsCount = computed(() => periods.value.length)
+
+function getPeriodName(schedule) {
+    return schedule['school_period']['is_custom'] ? schedule['school_period']['name'] : schedule['batch_subject']['subject']['full_name']
+}
+
+const subjects = [
+    'Mathematics',
+    'English',
+    'Science',
+    'History',
+    'Geography',
+    'Art',
+    'Music',
+    'Drama',
+    'Physical Education',
+    'Religious Education',
+    'French',
+    'Spanish',
+    'German',
+    'Latin',
+    'Mandarin',
+    'Japanese',
+    'Italian',
+    'Russian',
+    'Arabic',
+    'Hindi',
+    'Urdu',
+    'Bengali',
+    'Punjabi',
+]
 
 const tabs = [
+    'Overview',
     'Personal Details',
     'Guardian',
-    'Current Batch',
-
+    'Attendance',
     'Classes',
     'Notes'
 ]
@@ -85,5 +184,45 @@ const tabs = [
 </script>
 
 <style scoped>
+.note-preview {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    line-clamp: 2;
+    -webkit-box-orient: vertical;
+}
 
+.sessions {
+    grid-template-rows: repeat(v-bind(periodsCount), minmax(0, 1fr));
+}
+
+.hours {
+    grid-template-rows: repeat(v-bind((periodsCount + 1)), minmax(0, 1fr));
+}
+
+.days {
+    grid-template-rows: repeat(v-bind((periodsCount + 1)), minmax(0, 1fr));
+}
+
+.subjects {
+    grid-row-start: 2;
+    grid-row-end: v-bind(periodsCount + 2);
+}
+
+.session {
+    @apply border-r;
+}
+
+.sessions > .session:nth-last-child(-n + v-bind(periodsCount)) {
+    @apply border-r-0;
+}
+
+.day {
+    height: calc(100% * (1 / v-bind(periodsCount + 1)));
+}
+
+/*.sessions > .session:nth-child(9n) {*/
+/*    @apply border-b-0;*/
+/*}*/
 </style>
--- resources/js/Views/Students/StudentOverview.vue
+++ resources/js/Views/Students/StudentOverview.vue
@@ -11,7 +11,8 @@
             </div>
 
         </Card>
-        <Card icon :title="`Personal details`" class="!max-w-md">
+
+        <Card icon :title="`Personal details`" class="!grow">
 
             <ul class="flex w-full flex-col gap-2">
 
@@ -32,7 +33,11 @@
                 <li class="flex gap-2">
                     <RectangleStackIcon class="h-5 w-5 stroke-gray-500"/>
                     <span class="flex gap-1 text-sm">
-                    <span>12th Semester</span>
+                    <span class="flex">{{ student.batches.length }}
+                        <span v-if="student.batches.length === 1">st</span>
+                        <span class="ml-1">School Year</span>
+                        <span v-if="student.batches.length > 1">s</span>
+                    </span>
                     <span class="text-gray-500">at Gibson Youth</span>
                 </span>
                 </li>
@@ -62,12 +67,51 @@
 
         </Card>
 
+        <Card icon title="Guardian Contact" class="!max-w-sm">
+
+            <div class="flex w-full flex-col gap-6">
+
+                <div class="flex flex-col items-center gap-3">
+
+                    <div class="flex w-full items-center justify-between">
+                        <div class="flex flex-col">
+                            <Heading class="truncate whitespace-nowrap" size="sm">
+                                {{ guardian?.name }}
+                            </Heading>
+                            <Heading
+                                :class="{ 'text-gray-500': !!!student['guardian_relation'] }"
+                                class="whitespace-nowrap !font-normal" size="sm"
+                            >
+                                {{ student['guardian_relation'] || 'Guardian' }}
+                            </Heading>
+                        </div>
+                        <img
+                            :src="`https://xsgames.co/randomusers/avatar.php?g=${getOppositeGender(student.user.gender)}`"
+                            alt="avatar"
+                            class="aspect-square w-12 rounded-full"
+                        />
+                    </div>
+
+                    <div class="flex w-full gap-3">
+
+                        <TertiaryButton class="w-1/2">Contact</TertiaryButton>
+                        <TertiaryButton class="w-1/2">Details</TertiaryButton>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </Card>
+
     </div>
 
 </template>
 
 <script setup>
 import {computed} from "vue";
+import {getOppositeGender} from "@/fake.js"
 import Card from "@/Components/Card.vue"
 import Heading from "@/Components/Heading.vue";
 import {
@@ -78,11 +122,13 @@ import {
     DevicePhoneMobileIcon
 } from "@heroicons/vue/24/outline";
 import {useStudentStore} from "@/Store/students.js";
+import TertiaryButton from "@/Components/TertiaryButton.vue";
 
 const store = useStudentStore()
 
 const student = computed(() => store['student'])
 const activeBatch = computed(() => store['activeBatch'])
+const guardian = computed(() => store['student']['guardian']['user'])
 </script>
 
 <style scoped>
