2b03aba8b3af138e75dc02da29fd4856878a5c61
adilabdu <adilabdu68@gmail.com>
2023-04-24 17:16:29 +0300

    Query and returns the necessary data from the controller function to `students/{student}` page

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Level;
 use App\Models\Student;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -14,15 +15,29 @@ class StudentController extends Controller
 
     public function show(Student $student): Response
     {
-        $student = $student->load('user', 'guardian', 'guardian.user', 'batches', 'batches.batch', 'batches.batch.level');
-
-        $activeBatch = $student->batches()->whereHas('batch.schoolYear', function ($query) {
-            $query->where('end_date', null);
-        })->first();
+        $student = $student->load(
+            'user',
+            'guardian',
+            'guardian.user',
+            'batches',
+            'batches.batch',
+            'batches.batch.level',
+        );
 
         return Inertia::render('Students/Single', [
             'student' => $student,
-            'active_batch' => $activeBatch,
+            'schedule' => $student->activeBatch()->load(
+                'schedule:id,school_period_id,batch_subject_id,day_of_week,batch_id',
+                'schedule.batchSubject:id,teacher_id,subject_id,weekly_frequency',
+                'schedule.batchSubject.subject',
+                'schedule.schoolPeriod:id,name,start_time,duration,is_custom,level_category_id'
+            )->only('schedule')['schedule'],
+            'active_batch' => $student->activeBatch(load: ['level']),
+            'attendance' => [
+                'absence_rate' => $student->absenteePercentage(),
+                'absentee_records' => $student->absenteeRecords()->get(),
+            ],
+            'periods' => Level::find($student->activeBatch()->level->id)->levelCategory->schoolPeriods,
         ]);
     }
 }
