64c5b53cc6dc6d4a89926e42cea8c7df7177b523
adilabdu <adilabdu68@gmail.com>
2023-04-28 13:04:52 +0300

    Set up view and page SFCs for `levels/{level}` page
    - Entry page component is `Level/Index.vue`
    - View components renders inside are:
       - LevelSections
       - LevelStatistics
       - LevelStudents
       - LevelSubjects

---
+++ resources/js/Views/Levels/LevelSections.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <div class="grid grid-cols-4 gap-3">
+
+        <Card v-for="(batch, b) in batches" :key="b" class="!w-full" :title="`Section ${batch.section}`">
+
+            <div class="flex gap-3">
+
+                <div class="flex items-center gap-1">
+                    <div class="h-1.5 w-1.5 rounded-full bg-red-600"/>
+                    <span class="text-sm text-gray-500">{{ batch['students_count'] }} Students</span>
+                </div>
+
+                <div class="flex items-center gap-1">
+                    <div class="h-1.5 w-1.5 rotate-45 bg-yellow-600"/>
+                    <span class="text-sm text-gray-500">Teachers</span>
+                </div>
+
+                <div class="flex items-center gap-1">
+                    <div class="h-1.5 w-1.5 bg-green-600"/>
+                    <span class="text-sm text-gray-500">{{ batch['subjects_count'] }} Subjects</span>
+                </div>
+
+            </div>
+
+        </Card>
+
+    </div>
+
+</template>
+
+<script setup>
+import {computed} from "vue";
+import {usePage} from "@inertiajs/vue3";
+import Card from "@/Components/Card.vue";
+
+const batches = computed(() => usePage().props.batches)
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Views/Levels/LevelStudents.vue
@@ -0,0 +1,113 @@
+<template>
+
+    <div class="flex flex-col gap-3">
+
+        <TableElement
+            v-if="!! students"
+            :title="selectedSection && `Section ${selectedSection} Students List` || 'Students List'"
+            :subtitle="`Students enrolled in ${parseLevel(level.name)} for ${level.batches[0].school_year.name}`"
+            class="w-fit"
+            :row-actionable="true"
+            :selectable="false"
+            :footer="false"
+            :columns="studentsConfig"
+            :data="students"
+        >
+            <template #filter>
+                <RadioGroup v-model="selectedSection" :options="sectionsRadioButtons" name="sections" />
+            </template>
+
+            <template #row-actions="{ row }">
+                <Link :href="'/students/' + row.id" class="flex flex-col items-center gap-1">
+                    <EyeIcon class="h-3 w-3 stroke-2 transition-transform duration-150 hover:scale-125"/>
+                </Link>
+                <Link :href="'/users/' + row.id + '/edit'" class="flex flex-col items-center gap-1">
+                    <ArrowPathIcon
+                        class="h-3 w-3 stroke-2 transition-all duration-150 hover:scale-125 hover:stroke-blue-700"/>
+                </Link>
+                <Link :href="'/users/' + row.id + '/delete'" class="flex flex-col items-center gap-1">
+                    <ArchiveBoxXMarkIcon
+                        class="h-3 w-3 stroke-2 transition-all duration-150 hover:scale-125 hover:stroke-red-700"/>
+                </Link>
+            </template>
+        </TableElement>
+
+    </div>
+
+</template>
+
+<script setup>
+import {ref, computed, onMounted} from "vue";
+import {Link, router, usePage} from "@inertiajs/vue3";
+import TableElement from "@/Components/TableElement.vue";
+import moment from "moment";
+import RadioGroup from "@/Components/RadioGroup.vue";
+import {parseLevel} from "@/utils.js";
+import {ArchiveBoxXMarkIcon, ArrowPathIcon, EyeIcon} from "@heroicons/vue/24/outline/index.js";
+
+const level = computed(() => usePage().props.level)
+const students = computed(() => (usePage().props.students || [])
+    .filter(student => {
+        if (!! selectedSection.value) {
+            return student?.section === selectedSection.value
+        }
+        return true
+    }))
+const batches = computed(() => usePage().props.batches)
+
+const selectedSection = ref()
+const sectionsRadioButtons = computed(() => usePage().props.batches.map(batch => {
+    return {
+        label: `Section ${batch.section}`,
+        value: batch.section
+    }
+}))
+
+const studentsConfig = [
+    {
+        name: '',
+        key: 'name',
+        class: 'font-semibold',
+        align: 'right'
+    },
+    {
+        name: '',
+        key: 'email',
+        class: 'text-gray-500 text-xs w-full',
+        align: 'left'
+    },
+    {
+        name: '',
+        key: 'username',
+        class: 'text-gray-500 text-xs font-semibold'
+    },
+    {
+        name: '',
+        key: 'gender',
+        type: 'enum',
+        options: ['male', 'female']
+    },
+    {
+        name: 'Date of Birth',
+        key: 'date_of_birth',
+    },
+    {
+        name: 'Last updated',
+        key: 'updated_at',
+        class: 'text-gray-500 text-xs',
+        align: 'right'
+    }
+]
+
+onMounted(() => {
+
+    router.reload({
+        only: ["students"],
+    })
+
+})
+</script>
+
+<style scoped>
+
+</style>
