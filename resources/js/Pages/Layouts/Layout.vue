39537ece8131b4dc7a70500786e03ce5b30fb7ec
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-02 10:02:16 +0300

    Enhance layout with full-width support for teacher lesson plans route

--- resources/js/Pages/Layouts/Layout.vue
+++ resources/js/Pages/Layouts/Layout.vue
@@ -1,31 +1,41 @@
 <template>
-
     <div id="top-view"></div>
 
     <div class="relative flex h-screen w-full flex-col">
         <Header @open-drawer="drawerVisible = true"/>
         <div
-            class="w-full grow overflow-y-auto bg-white p-2 md:p-6">
-            <div
-                class="container mx-auto flex flex-col gap-12">
+            class="hide-scrollbar w-full grow overflow-y-auto bg-white p-2 md:p-2"
+            :class="teacherLessonPlanRoute ? 'teacher-lesson-plan-container' : 'container mx-auto flex flex-col gap-12'"
+        >
+            <div>
                 <slot/>
             </div>
         </div>
         <Notification/>
-
     </div>
-
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import {usePage} from "@inertiajs/vue3";
 import Header from "@/Views/Header.vue";
-import Drawer from "@/Views/Drawer.vue";
 import Notification from "@/Components/Notification.vue";
 
 const drawerVisible = ref(false);
+const page = usePage();
+
+const teacherLessonPlanRoute = computed(() =>
+    page.url.startsWith("/teacher/lesson-plan")
+);
 </script>
 
 <style scoped>
+.hide-scrollbar::-webkit-scrollbar {
+    display: none; /* for Chrome, Safari, and Opera */
+}
 
+.hide-scrollbar {
+    -ms-overflow-style: none; /* for Internet Explorer and Edge */
+    scrollbar-width: none; /* for Firefox */
+}
 </style>
