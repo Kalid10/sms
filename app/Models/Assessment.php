88ba163f4b38d6bcc2a5f46af3bb5338af609005
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-02 16:03:46 +0300

    Add assessment table and model with relationships -Run php artisan migrate

---
+++ app/Models/Assessment.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Assessment extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'assessment_type_id',
+        'batch_subject_id',
+        'quarter_id',
+        'due_date',
+    ];
+
+    public function assessmentType(): BelongsTo
+    {
+        return $this->belongsTo(AssessmentType::class);
+    }
+
+    public function batchSubject(): BelongsTo
+    {
+        return $this->belongsTo(BatchSubject::class);
+    }
+
+    public function quarter(): BelongsTo
+    {
+        return $this->belongsTo(Quarter::class);
+    }
+}
--- app/Models/AssessmentType.php
+++ app/Models/AssessmentType.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AssessmentType extends Model
@@ -29,5 +30,10 @@ class AssessmentType extends Model
     public function levelCategory(): BelongsTo
     {
         return $this->belongsTo(LevelCategory::class);
+    }
+
+    public function assessments(): HasMany
+    {
+        return $this->hasMany(Assessment::class);
     }
 }
