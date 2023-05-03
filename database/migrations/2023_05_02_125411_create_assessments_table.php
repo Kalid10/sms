5c4b08612788fae20d43b56c5bf71afe6437bdd2
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-03 14:02:08 +0300

    Add title, description and maximum_point column on assessments table

--- database/migrations/2023_05_02_125411_create_assessments_table.php
+++ database/migrations/2023_05_02_125411_create_assessments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('assessments', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->unsignedTinyInteger('maximum_point');
             $table->foreignId('assessment_type_id')->constrained()->cascadeOnDelete();
             $table->foreignId('batch_subject_id')->constrained()->cascadeOnDelete();
             $table->foreignId('quarter_id')->constrained()->cascadeOnDelete();
