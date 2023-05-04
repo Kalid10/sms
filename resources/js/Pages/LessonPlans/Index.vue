116e7d01c4ddcc270c1584ec363a6a5f2f8cc583
adilabdu <adilabdu68@gmail.com>
2023-05-04 19:22:36 +0300

    Finish up `teachers/lesson-plan` Page
    ###### New View Components
    - `LessonPlanCard`: Custom card to preview the lesson plans of a teacher in a week list
    - `LessonPLanFormModal`: Custom modal to view, update or add a teacher's lesson plan
    - `LessonPlanMonthViewer`: A container that holds the weekly views of a teacher's lesson plans
    - `LessonPlanWeekCard`: A custom Card that renders the list of a teacher's lesson plans in a given week
    
    ###### Updates
    - Call and render the new lesson plan view components inside `@/Pages/LessonPlans/Index`
    - Reset `p-2` for `teachers/lesson-plan` route inside `Layout` component
    - Sort a teacher's batch subjects by date when during query for `LessonPlans/Index` page
    - Eager load `level_category_id` of each batch's level
    
    ###### Additional Changes
    - New keyframes and animation classes `animate-slide-left` and `animate-darken` defined in `tailwind.config`. Implementation can be found on `@/Views/LessonPlans/LessonPlanFormModal` view component
    - UI component `Modal` can now accept `transparent` for `backgroundColor` prop data
    - New props `leading` for `TextArea` UI component to optionally set to `leading-loose` as used in `LessonPlanFormModal` (better implementation appreciated, currently this props can accept any valid css class)

--- resources/js/Pages/LessonPlans/Index.vue
+++ resources/js/Pages/LessonPlans/Index.vue
@@ -1,324 +1,91 @@
 <template>
-    <div class="flex min-h-screen justify-evenly space-x-3 md:px-10">
-        <div class="w-1/12">
-            <Heading class="text-xl">Subjects</Heading>
 
-            <div class="mt-5 flex flex-col space-y-3">
-                <div v-for="(item, key) in groupedSubjects" :key="key">
-                    <Heading class="mb-1">{{ item.level }} {{ item.section }}</Heading>
-                    <ul>
-                        <li
-                            v-for="subject in item.subjects" :key="subject.id" class="cursor-pointer hover:underline"
-                            @click="loadLessonPlan(subject)">
-                            {{ subject.name }}
-                        </li>
-                    </ul>
-                </div>
-            </div>
+    <div class="flex h-full max-h-full w-full flex-col overflow-auto bg-gray-50">
+
+        <div class="my-4 flex min-h-10 w-full items-center justify-between gap-2 px-8">
+            <h3 class="font-semibold">
+                <span class="font-normal">{{ months[parseInt(selectedMonth.split('-')[1]) - 1].label }}&nbsp;</span>
+                <span class="font-normal">Lesson Plans </span>
+                <span class="font-normal">for </span>
+                <span class="!bg-transparent">{{ parseLevel(batch.level.name) }}&nbsp;</span>
+                <span class="!bg-transparent">{{ batch.section }}&nbsp;</span>
+                <span class="">{{ selectedSubject }}</span>
+            </h3>
+            <SelectInput v-model="selectedMonth" class="min-w-[8rem] rounded-md border" placeholder="Months of the school year" :options="months" />
         </div>
-        <div class="w-10/12">
-            <div class="mb-4 flex flex-col items-center justify-between space-y-3 lg:mb-10 lg:space-y-0">
-                <Heading class="w-fit text-xl font-medium lg:text-3xl"> {{ moment(selectedMonth).format('MMMM') }}
-                    Lesson
-                    Plan For {{
-                        batch.level.name
-                    }} {{
-                        batch.section
-                    }} {{
-                        selectedSubject
-                    }}
-                </Heading>
-                <select
-                    v-model="selectedView"
-                    class="focus:ring/50 w-full rounded-md border-gray-300 text-center text-xs shadow-sm lg:hidden lg:w-fit lg:text-sm"
+
+        <div class="flex h-full max-h-full w-full grow flex-col">
+
+            <ul class="flex gap-3 border-b-2 px-8">
+
+                <li
+                    v-for="(subject, s) in subjects"
+                   :key="s"
                 >
-                    <option v-for="option in views" :key="option" :value="option.value">{{ option.name }}</option>
-                </select>
-                <div class="flex items-center space-x-3">
-                    <label for="month-selector" class="hidden lg:block">Select Month:</label>
-                    <select
-                        id="month-selector"
-                        v-model="selectedMonth"
-                        class="focus:ring/50 w-full rounded-md border-gray-300 text-center text-xs shadow-sm lg:w-fit lg:text-sm"
-                        @change="handleMonthChange"
+                    <Link
+                        :href="`/teacher/lesson-plan?batch_subject_id=${subject.id}&month=${selectedMonth}`"
+                        :class="lessonPlanSubject.batch.level.name === subject.batch.level.name && lessonPlanSubject.batch.section === subject.batch.section ? 'border-x-2 border-t-2 bg-white text-black' : 'text-gray-500'"
+                        class="relative grid min-w-[4rem] place-items-center rounded-t py-2.5 px-8 text-center text-sm font-medium"
                     >
-                        <option v-for="month in months" :key="month.value" :value="month.value">{{
-                                month.label
-                            }}
-                        </option>
-                    </select>
-                </div>
-            </div>
 
-            <div class="mt-5 flex min-h-screen flex-col text-center md:grid" :class="'grid-cols-'+ weeksInMonth">
-                <div v-for="(item, index) in views" :key="index" class="h-full border-r border-gray-300">
-                    <Heading class="my-4 md:my-0">{{ item.name }}</Heading>
-                    <div class="flex flex-col space-y-3 lg:space-y-4 xl:space-y-7">
-                        <div
-                            v-for="batchSession in batchSessions[item.value]" :key="batchSession.id"
-                            class="m-2 h-full bg-neutral-50 text-center">
-                            <div v-if="batchSession.lesson_plan" class="text-center">
+                    <span class="flex items-end">
+                        <span>{{ parseLevel(subject.batch.level.name) }} {{ subject.batch.section }}</span>
+                        &nbsp;
+                        <span>{{ subject.subject.full_name }}</span>
+                    </span>
 
-                                <Card
-                                    class="min-w-full py-2 text-center">
-                                    <div class="font-semibold">
-                                        {{ moment(batchSession.date).format('dddd MMMM Do') }}, Period
-                                        {{ batchSession.batch_schedule.school_period.name }}
-                                    </div>
-                                    <div>
-                                        {{ batchSession.lesson_plan.topic }}
+                        <div v-if="lessonPlanSubject.batch.level.name === subject.batch.level.name && lessonPlanSubject.batch.section === subject.batch.section" class="absolute bottom-0 h-[2px] w-full translate-y-full bg-white"></div>
 
-                                    </div>
+                    </Link>
+                </li>
 
-                                    <SecondaryButton
-                                        class="bg-neutral-600 text-white" title="View Lesson Plan"
-                                        @click="prepareForm(batchSession.id)"/>
-                                </Card>
-                            </div>
-                            <div v-else>
-                                <Card
-                                    class="min-w-full"
-                                >
-                                    <div class="font-semibold">
-                                        {{ moment(batchSession.date).format('dddd MMMM Do') }}, Period
-                                        {{ batchSession.batch_schedule.school_period.name }}
-                                    </div>
-                                    <div class="font-bold">
-                                        No lesson plan found!
-                                    </div>
-                                    <PrimaryButton title="Add Lesson Plan" @click="prepareForm(batchSession.id)"/>
-                                </Card>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </ul>
+            <LessonPlanMonthViewer @select="selectBatchSession" />
 
-            <div>
-                <Modal v-model:view="isModalOpen">
-                    <div class="fixed inset-y-0 right-0 w-7/12 rounded-l-lg bg-white">
-
-
-                        <div class="flex flex-col space-y-7">
-                            <div class="bg-white py-5 pl-7">
-                                <div>
-                                    <h1 class="font-light italic"> Grade {{ batch.level.name }} {{ batch.section }}</h1>
-                                </div>
-                                <div class="text-2xl font-bold">
-                                    {{ moment(selectedBatchSession.date).format('dddd MMMM Do') }}, Period
-                                    {{ selectedBatchSession.batch_schedule.school_period.name }}
-                                </div>
-
-                            </div>
-
-                            <div class="flex justify-evenly">
-                                <div
-                                    class="border-1 flex w-6/12 flex-col space-y-5 rounded-md border-black bg-white">
-                                    <Heading>Add Lesson Plan</Heading>
-                                    <TextInput v-model="form.topic" placeholder="Topic" title=""/>
-                                    <TextArea
-                                        v-model="form.description" placeholder="Description" label=""
-                                        :rows="isLg?18:7"/>
-
-                                    <div class="flex w-full justify-end">
-                                        <PrimaryButton title="Submit" class="w-1/5" @click="handleSubmit"/>
-                                    </div>
-
-
-                                    <div class="mt-10">
-                                        <Heading>Homeworks and ClassWorks</Heading>
-                                    </div>
-                                </div>
-
-                                <div class=" w-5/12 ">
-
-
-                                    <Heading class="fixed">Previous Lesson Plans
-                                    </Heading>
-                                    <div class="hide-scrollbar mt-10 h-screen overflow-y-auto pb-44">
-                                        <div>
-                                            <div
-                                                v-if="previousBatchSessionsWithLessonPlans.length > 0"
-                                                class=" flex flex-col space-y-4">
-                                                <div
-                                                    v-for="previousBatchSession in previousBatchSessionsWithLessonPlans"
-                                                    :key="previousBatchSession.id"
-                                                    class="flex flex-col space-y-3">
-                                                    <div class="font-light italic">
-                                                        {{ moment(previousBatchSession.date).format('dddd MMMM Do') }},
-                                                        Period
-                                                        {{ previousBatchSession.batch_schedule.school_period.name }}
-                                                    </div>
-                                                    <div class="font-semibold">
-                                                        {{ previousBatchSession.lesson_plan.topic }}
-                                                    </div>
-                                                    <div class="text-sm font-light">
-                                                        {{ previousBatchSession.lesson_plan.description }}
-                                                    </div>
-                                                    <hr class="my-4">
-                                                </div>
-                                            </div>
-                                            <div v-else>
-                                                No previous lesson plans found.
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Modal>
-            </div>
         </div>
+
     </div>
+    <LessonPlanFormModal v-model:view="openForm" :batch-session="selectedBatchSession" />
+
 </template>
+
 <script setup>
-import {computed, ref} from 'vue';
-import {router, useForm, usePage} from '@inertiajs/vue3';
-import moment from 'moment';
-import Heading from '@/Components/Heading.vue';
-import Card from '@/Components/Card.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import SecondaryButton from '@/Components/SecondaryButton.vue';
-import Modal from '@/Components/Modal.vue';
-import TextArea from '@/Components/TextArea.vue';
-import TextInput from '@/Components/TextInput.vue';
+import {computed, ref, watch} from "vue";
+import Card from "@/Components/Card.vue";
+import {usePage, Link, router} from "@inertiajs/vue3";
+import {levelCategoryLabels, parseLevel} from "@/utils.js";
+import { PlusCircleIcon, ExclamationTriangleIcon, ClockIcon, CalendarIcon } from "@heroicons/vue/24/outline";
+import moment from "moment";
+import LessonPlanFormModal from "@/Views/LessonPlans/LessonPlanFormModal.vue";
+import SelectInput from "@/Components/SelectInput.vue";
+import LessonPlanMonthViewer from "@/Views/LessonPlans/LessonPlanMonthViewer.vue";
 
-const batchSessions = computed(() => usePage().props.batch_sessions);
-const batch = computed(() => usePage().props.batch);
-const weeksInMonth = usePage().props.weeks_in_month;
-const subjects = computed(() => usePage().props.subjects);
-const selectedSubject = ref(usePage().props.lesson_plan_subject.subject.full_name);
-const isModalOpen = ref(false);
-const isLg = computed(() => window.innerWidth >= 1024);
-const selectedMonth = ref(usePage().props.selected_month);
-const selectedView = ref('week1');
-const months = usePage().props.months;
-
-const groupedSubjects = computed(() => {
-    const grouped = {};
-
-    subjects.value.forEach((subject) => {
-        const batch = subject.batch;
-        const levelName = batch.level.name;
-        const section = batch.section;
-        const key = `${levelName}-${section}`;
-
-        if (!grouped[key]) {
-            grouped[key] = {
-                level: levelName,
-                section: section,
-                subjects: [],
-            };
-        }
-
-        grouped[key].subjects.push({
-            id: subject.id,
-            name: subject.subject.full_name,
-        });
-    });
-    return grouped;
-});
-
-const previousBatchSessionsWithLessonPlans = computed(() => {
-    if (selectedBatchSession.value) {
-        const allBatchSessions = Object.values(batchSessions.value).flat();
-        const currentBatchSessionIndex = allBatchSessions.findIndex(bs => bs.id === selectedBatchSession.value.id);
-
-        const sessionsWithLessonPlans = [];
-        for (let i = currentBatchSessionIndex - 1; i >= 0; i--) {
-            if (allBatchSessions[i].lesson_plan) {
-                sessionsWithLessonPlans.push(allBatchSessions[i]);
-            }
-        }
-        return sessionsWithLessonPlans;
-    }
-    return [];
-});
-
-const views = computed(() => {
-    let views = [];
-    for (let i = 1; i <= weeksInMonth; i++) {
-        views.push({
-            name: `Week ${i}`,
-            value: `week${i}`,
-        });
-    }
-    return views;
-});
-
-const form = useForm({
-    batch_session_id: '',
-    topic: '',
-    description: ''
-});
-
-const selectedBatchSession = ref(null);
-
-function prepareForm(batchSessionId) {
-    let batchSession = null;
-
-    // Find the batch session in all weeks
-    for (const weekKey in batchSessions.value) {
-        batchSession = batchSessions.value[weekKey].find(bs => bs.id === batchSessionId);
-        if (batchSession) {
-            break;
-        }
-    }
-
-    // If the batch session is found
-    if (batchSession) {
-        // Set the form data based on the existing lesson plan or a new one
-        if (batchSession.lesson_plan) {
-            form.topic = batchSession.lesson_plan.topic;
-            form.description = batchSession.lesson_plan.description;
-        } else {
-            form.topic = '';
-            form.description = '';
-        }
-
-        form.batch_session_id = batchSessionId;
-        isModalOpen.value = true;
-
-        selectedBatchSession.value = batchSession;
-    } else {
-        console.error('Batch session not found in any week');
-    }
-}
+const batchSessions = computed(() => usePage().props['batch_sessions'])
+const batch = computed(() => usePage().props.batch)
+const subjects = computed(() => usePage().props.subjects)
+const lessonPlanSubject = computed(() => usePage().props.lesson_plan_subject)
+const months = computed(() => usePage().props.months || [])
+const selectedMonth = ref(usePage().props.selected_month)
+const selectedSubject = computed(() => usePage().props.lesson_plan_subject.subject.full_name);
 
 function handleMonthChange() {
-    router.get('/teacher/lesson-plan/', {
+    router.get('/teacher/lesson-plan', {
         batch_subject_id: selectedSubject.value.id,
         month: selectedMonth.value,
     });
 }
 
-function handleSubmit() {
-    form.post('/teacher/lesson-plan', {
-        onSuccess: () => {
-            form.reset();
-            isModalOpen.value = false;
-        }
-    })
-}
+watch(selectedMonth, handleMonthChange)
 
-function loadLessonPlan(item) {
-    selectedSubject.value = item.name;
-    router.get('/teacher/lesson-plan/', {
-        batch_subject_id: item.id,
-        month: selectedMonth.value,
-    });
+const openForm = ref(false)
+const selectedBatchSession = ref(null)
+
+function selectBatchSession(session) {
+    selectedBatchSession.value = session
+    openForm.value = true
 }
 </script>
 
+<style scoped>
 
-<style>
-.hide-scrollbar::-webkit-scrollbar {
-    display: none; /* for Chrome, Safari, and Opera */
-}
-
-.hide-scrollbar {
-    -ms-overflow-style: none; /* for Internet Explorer and Edge */
-    scrollbar-width: none; /* for Firefox */
-}
 </style>
---
+++ resources/js/Views/LessonPlans/LessonPlanCard.vue
@@ -0,0 +1,84 @@
+<template>
+
+    <Card
+        :class="!! session['lesson_plan_id'] ?
+        'bg-white border-gray-300 !shadow-sm' :
+        '!bg-transparent !border-gray-300 !border-dashed border-2 !shadow-none'"
+        class="!w-full cursor-pointer"
+    >
+
+        <div class="group flex flex-col gap-4">
+
+            <div v-if="!! session['lesson_plan_id']" class="flex flex-col gap-0.5">
+
+                <h3 class="text-light text-xs text-gray-500">{{ `Lesson Plan #${Math.floor(Math.random() * 100)}` }}</h3>
+
+                <h3 class=" cursor-pointer text-sm font-medium underline-offset-2 hover:underline">
+                    {{ session['lesson_plan']['topic'] }}
+                </h3>
+
+            </div>
+
+            <div v-else class="w-full py-2">
+
+                <div class="flex w-full flex-col items-center justify-center gap-1">
+                    <PlusCircleIcon class="h-6 w-6 text-gray-500"/>
+                    <h3 class="text-xs font-semibold text-black">Add Lesson Plan</h3>
+                    <h3 class="max-w-[18rem] text-center text-xs font-medium text-gray-500">
+                        Lesson plan has not been added for this session. Add a new session
+                        before the session starts.
+                    </h3>
+                </div>
+
+            </div>
+
+            <div class="flex items-end justify-between">
+                <div class="flex origin-bottom-left scale-[.8] items-center gap-2">
+                    <span class="rounded-xl border border-black bg-white px-2 py-0.5 text-xs font-semibold">
+                        {{ session['batch_schedule']['day_of_week'].substring(0,3).toUpperCase() }}
+                    </span>
+                    <h3 class="font-semibold text-gray-500">{{ moment(session['date']).format('MMMM Do') }}</h3>
+                </div>
+
+                <div class="flex origin-bottom-right scale-90 items-center gap-1.5">
+                    <div class="flex gap-1">
+
+                        <div class="flex items-end gap-1.5">
+                            <span class="text-sm font-semibold text-gray-500">Period</span>
+                            <div class="flex origin-left scale-95 items-center gap-1">
+                                <span class="grid h-5 w-5 place-items-center rounded-full border border-brand-100 bg-brand-50 text-xs font-semibold leading-none text-brand-100">
+                                    {{ session['batch_schedule']['school_period']['name'] }}
+                                </span>
+                            </div>
+                        </div>
+
+                        <h3 class="text-sm font-semibold text-gray-500">
+                            {{ moment(session['batch_schedule']['school_period']['start_time'], 'HH:mm:ss').format('HH:mm A') }}
+                        </h3>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </Card>
+
+</template>
+
+<script setup>
+import moment from "moment";
+import Card from "@/Components/Card.vue";
+import {PlusCircleIcon} from "@heroicons/vue/24/outline/index.js";
+
+const props = defineProps({
+    session: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
