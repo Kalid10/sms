10eb77a5f53c2f7d903cc5665a34bf6b6f7a1866
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-10 12:23:31 +0300

    Add relationships with level and school year on batch student model

--- app/Models/BatchStudent.php
+++ app/Models/BatchStudent.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
@@ -26,6 +27,30 @@ class BatchStudent extends Model
     public function student(): BelongsTo
     {
         return $this->belongsTo(Student::class);
+    }
+
+    public function schoolYear(): HasOneThrough
+    {
+        return $this->hasOneThrough(
+            SchoolYear::class,
+            Batch::class,
+            'id',
+            'id',
+            'batch_id',
+            'school_year_id'
+        );
+    }
+
+    public function level(): HasOneThrough
+    {
+        return $this->hasOneThrough(
+            Level::class,
+            Batch::class,
+            'id',
+            'id',
+            'batch_id',
+            'level_id'
+        );
     }
 
     public function getActivitylogOptions(): LogOptions
