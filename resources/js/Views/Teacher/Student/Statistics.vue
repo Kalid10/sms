e05b12a425f0161ca647d2b91d4d6329b30899d1
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-16 17:38:24 +0300

    Add statistics view

---
+++ resources/js/Views/Teacher/Student/Statistics.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex w-full justify-between">
+        <StatCard
+            title="Grade"
+            color="blue"
+            value="50/60"
+            :icon="ChartBarIcon"
+        />
+        <StatCard
+            title="Attendance"
+            color="red"
+            value="80%"
+            :icon="CalendarDaysIcon"
+        />
+        <StatCard
+            title="Conduct"
+            color="emerald"
+            value="C"
+            :icon="HandThumbUpIcon"
+        />
+        <StatCard
+            title="Conduct"
+            color="emerald"
+            value="C"
+            :icon="HandThumbUpIcon"
+        />
+    </div>
+</template>
+<script setup>
+import StatCard from "@/Components/StatCard.vue";
+import {
+    CalendarDaysIcon,
+    ChartBarIcon,
+    HandThumbUpIcon,
+} from "@heroicons/vue/24/outline";
+</script>
+<style scoped></style>
