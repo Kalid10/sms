890de7e909b1129c585d304c7bf3c8e5d161fda6
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-16 17:48:14 +0300

    Add student info page for teachers

--- resources/js/Pages/Teacher/Student.vue
+++ resources/js/Pages/Teacher/Student.vue
@@ -1,43 +1,98 @@
 <template>
-    <div class="flex w-full flex-col px-2 py-3 lg:space-y-4 lg:px-12 lg:py-4">
-        <!-- Header-->
-        <div
-            class="flex items-start rounded-lg py-3 text-xl font-light lg:py-4 lg:pl-0 lg:text-4xl"
-        >
-            <span class="font-semibold">
-                {{ student.user.name }}
-            </span>
+    <div class="flex min-h-screen w-full justify-between">
+        <!--        Left side-->
+        <div class="mx-10 flex w-8/12 grow flex-col space-y-4 py-4 2xl:py-6">
+            <!--        Header-->
+            <div
+                class="flex flex-col items-start justify-between space-y-1 rounded-lg"
+            >
+                <span class="text-xl font-semibold lg:text-4xl">
+                    {{ student.user.name }}</span
+                >
+                <span class="text-sm">{{
+                    moment().format(" dddd MMMM D YYYY")
+                }}</span>
+            </div>
+
+            <!--        Statistics-->
+            <div class="flex h-32 w-full items-center justify-between">
+                <Statistics />
+            </div>
+
+            <!--        Assessments and NextClass section-->
+            <div
+                class="flex h-full w-full flex-col items-center justify-center space-y-6"
+            >
+                <div class="flex w-full justify-between pt-10 pb-2">
+                    <!--           Assessments section-->
+                    <div class="w-full lg:w-7/12">
+                        <Assessments />
+                    </div>
+
+                    <!--            NextClass section-->
+                    <div
+                        class="flex h-full w-full flex-col items-center justify-evenly rounded-lg text-white md:w-9/12 lg:w-4/12"
+                        :class="
+                            isNextClassSubjectTeacher
+                                ? 'bg-gradient-to-r from-neutral-900 to-stone-800'
+                                : 'bg-black'
+                        "
+                    >
+                        <NextClass
+                            :next-class="upcomingSession"
+                            view="student"
+                        />
+                    </div>
+                </div>
+                <div
+                    class="w-full cursor-pointer text-end text-sm font-light underline underline-offset-2 hover:font-medium"
+                    @click="showClassSchedule = !showClassSchedule"
+                >
+                    <span v-if="showClassSchedule">Hide</span>
+                    <span v-else>Show</span> Class Schedule
+                </div>
+                <div class="w-full grow">
+                    <StudentSemesterSchedule
+                        v-if="showClassSchedule"
+                        class="h-full w-full"
+                    />
+                </div>
+            </div>
         </div>
 
+        <!--        Right side-->
         <div
-            class="flex w-full flex-col justify-center space-y-6 md:flex-row md:justify-between lg:space-y-0"
+            class="flex min-h-screen w-4/12 flex-col items-center space-y-7 border-l pb-4 pt-5"
         >
-            <div class="h-fit w-full rounded-lg md:w-1/2">
-                <Assessments />
-            </div>
-            <div
-                class="flex h-full w-2/3 flex-col items-center justify-evenly rounded-xl text-white md:w-4/12 lg:w-3/12"
-                :class="
-                    isNextClassSubjectTeacher
-                        ? 'bg-gradient-to-r from-neutral-900 to-stone-800'
-                        : 'bg-black'
-                "
-            >
-                <NextClass :next-class="upcomingSession" view="student" />
-            </div>
+            <Information />
+
+            <div class="h-[0.02rem] w-11/12 bg-gray-200/90"></div>
+
+            <Rank />
+
+            <div class="h-[0.02rem] w-11/12 bg-gray-200/90"></div>
+
+            <Notes />
         </div>
     </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { usePage } from "@inertiajs/vue3";
 import NextClass from "@/Views/Teacher/NextClass/NextClass.vue";
 import Assessments from "@/Views/Teacher/Student/Assessments.vue";
+import Statistics from "@/Views/Teacher/Student/Statistics.vue";
+import moment from "moment";
+import Rank from "@/Views/Teacher/Student/Rank.vue";
+import StudentSemesterSchedule from "@/Views/Students/StudentSemesterSchedule.vue";
+import Notes from "@/Views/Teacher/Student/Notes.vue";
+import Information from "@/Views/Teacher/Student/GuardianInformation.vue";
 
 const student = computed(() => usePage().props.student);
-const batchSessions = computed(() => usePage().props.batchSessions);
+const batchSessions = computed(() => usePage().props.batch_sessions);
 const teacher = usePage().props.auth.user.teacher;
+const showClassSchedule = ref(false);
 
 // Get the first batch session from batchSessions where batchSubject id is not null
 const upcomingSession = computed(() => {
