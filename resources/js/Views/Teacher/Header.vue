246af15b97d24e815e4d2b833deb09bec491afd6
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-19 18:08:45 +0300

    Update header view to accept title props

--- resources/js/Views/Teacher/Header.vue
+++ resources/js/Views/Teacher/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex h-full flex-col items-start justify-evenly rounded-lg">
-        <span class="text-xl font-semibold lg:text-4xl"> My Classes</span>
+        <span class="text-xl font-semibold lg:text-4xl"> {{ title }}</span>
         <span class="text-xs font-light">{{
             moment().format(" dddd MMMM D YYYY")
         }}</span>
@@ -9,6 +9,13 @@
 
 <script setup>
 import moment from "moment";
+
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+});
 </script>
 
 <style scoped></style>
