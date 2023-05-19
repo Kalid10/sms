f458562cb2788a86cdf6c340beb43abf3a51bd47
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-19 16:43:19 +0300

    Add header view on batches page

---
+++ resources/js/Views/Teacher/Batches/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex h-full flex-col items-start justify-evenly rounded-lg">
+        <span class="text-xl font-semibold lg:text-4xl"> My Classes</span>
+        <span class="text-xs font-light">{{
+            moment().format(" dddd MMMM D YYYY")
+        }}</span>
+    </div>
+</template>
+
+<script setup>
+import moment from "moment";
+</script>
+
+<style scoped></style>
