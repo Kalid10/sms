941bb2b95a4bc45707739f7c20025c45629368ef
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-20 10:11:11 +0300

    Add StudentScoreList view

---
+++ resources/js/Views/Teacher/Assessments/Details/StudentScoreList.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="flex w-full flex-col space-y-3 pt-5">
+        <div class="flex w-full items-center">
+            <StudentsScoreSection
+                label="Exemplary"
+                :icon="ArrowTrendingUpIcon"
+                icon-color="text-emerald-500"
+                :students="exemplaryStudents"
+            />
+            <div class="h-full w-[0.01rem] bg-gray-200"></div>
+            <StudentsScoreSection
+                label="UnderAchievers"
+                :icon="ArrowSmallDownIcon"
+                icon-color="text-red-600"
+                :students="underAchievingStudents"
+            />
+        </div>
+        <LinkCell
+            href="/score"
+            class="w-full pr-2 text-end"
+            value="View All Results"
+        />
+    </div>
+</template>
+
+<script setup>
+import StudentsScoreSection from "@/Views/Teacher/Assessments/Details/StudentsScoreSection.vue";
+import {
+    ArrowSmallDownIcon,
+    ArrowTrendingUpIcon,
+} from "@heroicons/vue/24/outline";
+import LinkCell from "@/Components/LinkCell.vue";
+
+const exemplaryStudents = [
+    {
+        id: 1,
+        name: "Binyam Lemma",
+        score: "20",
+        total: "20",
+    },
+    {
+        id: 2,
+        name: "Kalid Abdu",
+        score: "18",
+        total: "20",
+    },
+    {
+        id: 3,
+        name: "Samuel Mesfin",
+        score: "18",
+        total: "20",
+    },
+    {
+        id: 4,
+        name: "Meron Tadesse",
+        score: "17",
+        total: "20",
+    },
+];
+
+const underAchievingStudents = [
+    {
+        id: 5,
+        name: "Tsega Molla",
+        score: "3",
+        total: "20",
+    },
+    {
+        id: 6,
+        name: "Fasil Tesfaye",
+        score: "4",
+        total: "20",
+    },
+    {
+        id: 7,
+        name: "Kebede Getachew",
+        score: "7",
+        total: "20",
+    },
+    {
+        id: 8,
+        name: "Ermias Belay",
+        score: "10",
+        total: "20",
+    },
+];
+</script>
---
+++ resources/js/Views/Teacher/Assessments/Details/StudentsScoreSection.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="flex w-1/2 flex-col items-center space-y-5">
+        <div class="flex space-x-2">
+            <component :is="icon" class="w-5" :class="iconColor" />
+            <div class="font-bold">{{ label }}</div>
+        </div>
+        <div
+            v-for="(student, index) in students"
+            :key="student.id"
+            class="w-full"
+            :class="index % 2 === 0 ? 'bg-gray-200/90' : ''"
+        >
+            <div class="flex h-10 w-full items-center justify-evenly">
+                <div
+                    class="flex h-full w-6/12 cursor-pointer items-center justify-center text-[0.65rem] underline-offset-2 hover:font-semibold hover:underline"
+                >
+                    {{ student.name }}
+                </div>
+                <ResultCard
+                    :score="student.score"
+                    :total="student.total"
+                    size="sm"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import ResultCard from "@/Views/Teacher/Assessments/Details/ResultCard.vue";
+
+defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: Object,
+        required: true,
+    },
+    iconColor: {
+        type: String,
+        default: "",
+    },
+    students: {
+        type: Array,
+        required: true,
+    },
+});
+</script>
