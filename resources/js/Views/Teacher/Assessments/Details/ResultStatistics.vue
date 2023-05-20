42dfd4113bd6b096668a6d6c9814f4a1d597f214
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-20 10:08:54 +0300

    Add result card and statistics components

---
+++ resources/js/Views/Teacher/Assessments/Details/ResultStatistics.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex w-full items-center justify-between">
+        <ResultCard label="Average Result" score="13.5" total="20" />
+        <div class="h-14 w-[0.01rem] bg-gray-200"></div>
+        <ResultCard label="Highest Result" score="20" total="20" />
+        <div class="h-14 w-[0.01rem] bg-gray-200"></div>
+        <ResultCard label="Lowest Result" score="3.5" total="20" />
+    </div>
+</template>
+
+<script setup>
+import ResultCard from "@/Views/Teacher/Assessments/Details/ResultCard.vue";
+</script>
