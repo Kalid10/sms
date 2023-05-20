ac44b09d390f70fd56c9dd9db8b61a5648dffac0
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-20 10:15:04 +0300

    Update assessment table view

--- resources/js/Views/Teacher/Assessments/Table/Index.vue
+++ resources/js/Views/Teacher/Assessments/Table/Index.vue
@@ -1,10 +1,12 @@
 <template>
     <div v-if="filteredAssessments">
         <TableElement
-            class="h-full text-black"
+            class="h-full bg-white text-black !shadow-none"
             :data="filteredAssessments"
             :selectable="false"
             :columns="config"
+            header-style="!bg-black text-white text-xs"
+            :footer="false"
         >
             <template #filter>
                 <Filters />
@@ -15,7 +17,7 @@
 <script setup>
 import { usePage } from "@inertiajs/vue3";
 import TableElement from "@/Components/TableElement.vue";
-import { computed } from "vue";
+import { capitalize, computed } from "vue";
 import moment from "moment";
 import Filters from "@/Views/Teacher/Assessments/Table/Filters.vue";
 
@@ -25,10 +27,9 @@ const filteredAssessments = computed(() => {
         return {
             title: assessment.title,
             max_points: assessment.maximum_point,
-            due_date: moment(assessment.due_date).format("dddd, MMMM D YYYY"),
-            updated_at: moment(assessment.updated_at).format(
-                "dddd, MMMM D YYYY"
-            ),
+            status: capitalize(assessment.status),
+            due_date: moment(assessment.due_date).fromNow(),
+            updated_at: moment(assessment.updated_at).fromNow(),
         };
     });
 });
@@ -37,18 +38,30 @@ const config = [
     {
         name: "Title",
         key: "title",
+        class: "text-xs",
+        link: "/teacher/assessments/1",
     },
     {
         name: "Max Points",
         key: "max_points",
+        class: "text-xs",
+    },
+    {
+        name: "Status",
+        key: "status",
+        type: "enum",
+        options: ["Marking", "Completed", "Published", "Draft", "Closed"],
+        class: "text-xs",
     },
     {
         name: "Due Date",
         key: "due_date",
+        class: "text-xs",
     },
     {
         name: "Last Update",
         key: "updated_at",
+        class: "text-xs opacity-70",
     },
 ];
 </script>
