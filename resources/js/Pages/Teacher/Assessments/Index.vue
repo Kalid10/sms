51b65debd459e2542e806e83d19ff19451f17374
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-20 10:13:56 +0300

    Organize the assessments into separate view

--- resources/js/Pages/Teacher/Assessments/Index.vue
+++ resources/js/Pages/Teacher/Assessments/Index.vue
@@ -1,27 +1,62 @@
 <template>
-    <div
-        class="flex min-h-screen w-full flex-col space-y-4 bg-red-400 px-5 pb-4 pt-8"
-    >
-        <div class="font-medium lg:text-4xl">Assessments</div>
+    <div class="flex min-h-screen w-full justify-between px-5 pb-4 pt-8">
+        <div class="flex w-7/12 flex-col space-y-6 pl-2 pr-6">
+            <div class="font-medium lg:text-4xl">Assessments</div>
 
-        <PrimaryButton
-            class="w-full lg:w-1/4"
-            value="Create Assessment"
-            @click="showModal = true"
-            >Create Assessment
-        </PrimaryButton>
-        <Modal v-model:view="showModal">
-            <Form @success="showModal = false" />
-        </Modal>
-        <Table />
+            <div
+                class="w-fit cursor-pointer px-2 text-xs underline underline-offset-2 hover:font-medium"
+                @click="showModal = true"
+            >
+                Create Assessment
+            </div>
+            <Modal v-model:view="showModal">
+                <Form @success="showModal = false" />
+            </Modal>
+
+            <div class="flex h-fit justify-between">
+                <Table class="w-full" />
+            </div>
+        </div>
+        <div class="h-full w-[0.01rem] bg-gray-200"></div>
+
+        <div class="flex w-5/12 flex-col space-y-8 pl-5">
+            <Header
+                title="Test 1 Biology Grade 11A"
+                :formatted-date="
+                    String(moment('05/10/2023').format('dddd MMMM DD YYYY'))
+                "
+            />
+
+            <GeneralInfo
+                teacher-name="Bereket Gobeze"
+                count="32"
+                status="Marking"
+            />
+
+            <ResultStatistics />
+
+            <StudentScoreList />
+
+            <div class="flex w-full items-center justify-center">
+                <SecondaryButton
+                    title="Start Marking"
+                    class="w-3/12 rounded-xl bg-emerald-500 text-xs text-white"
+                />
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
 import Table from "@/Views/Teacher/Assessments/Table/Index.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
 import Modal from "@/Components/Modal.vue";
 import Form from "@/Views/Teacher/Assessments/Form.vue";
 import { ref } from "vue";
+import moment from "moment/moment";
+import Header from "@/Views/Teacher/Assessments/Details/Header.vue";
+import GeneralInfo from "@/Views/Teacher/Assessments/Details/Info.vue";
+import ResultStatistics from "@/Views/Teacher/Assessments/Details/ResultStatistics.vue";
+import StudentScoreList from "@/Views/Teacher/Assessments/Details/StudentScoreList.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
 
 const showModal = ref(false);
 </script>
---
+++ resources/js/Views/Teacher/Assessments/Details/Info.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex w-full justify-between">
+        <div class="flex h-20 w-6/12 flex-col items-center justify-evenly">
+            <div class="text-4xl font-bold">{{ count }}</div>
+            <div
+                class="cursor-pointer text-xs font-light underline-offset-2 hover:font-medium hover:underline"
+            >
+                Total Students
+            </div>
+        </div>
+        <div class="h-full w-[0.01rem] bg-gray-200"></div>
+        <div class="flex w-6/12 flex-col items-center justify-evenly">
+            <div
+                class="w-fit rounded-xl bg-emerald-500 px-2 py-1 text-end text-xs text-white"
+            >
+                {{ status }}
+            </div>
+            <div
+                class="mt-2 cursor-pointer text-xs font-light underline-offset-2 hover:font-semibold hover:underline"
+            >
+                Mr. {{ teacherName }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    count: {
+        type: String,
+        required: true,
+    },
+    teacherName: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        default: "Marking",
+    },
+});
+</script>
+
+<style scoped></style>
