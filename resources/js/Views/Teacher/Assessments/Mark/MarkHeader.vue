10f0dbe4e0bad80dab3fb3faa39f9116eca2ebb3
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-23 15:22:30 +0300

    Add mark header and mark item views

---
+++ resources/js/Views/Teacher/Assessments/Mark/MarkHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <Header :is-date-open="false" :title="'Mark Students'" class="w-fit" />
+    <div class="flex h-fit w-fit items-center space-x-1 text-xs font-light">
+        <span>{{ grade }} </span>
+        <span>{{ assessment.batch_subject.subject.full_name }}</span>
+        <span> {{ assessment.title }}</span>
+    </div>
+</template>
+<script setup>
+import Header from "@/Views/Teacher/Header.vue";
+import { usePage } from "@inertiajs/vue3";
+
+const assessment = usePage().props.assessment;
+const grade =
+    assessment.batch_subject.batch.level.name +
+    assessment.batch_subject.batch.section;
+</script>
+
+<style scoped></style>
---
+++ resources/js/Views/Teacher/Assessments/Mark/MarkItem.vue
@@ -0,0 +1,135 @@
+<template>
+    <div class="flex w-full flex-col space-y-2 pb-6">
+        <div
+            v-for="(item, index) in assessment.students"
+            :key="index"
+            class="flex w-full cursor-pointer justify-between rounded-md pl-2 text-sm hover:bg-gray-50"
+            :class="{
+                'blur-effect hover:bg-white':
+                    isInputFocused && focusedInputIndex !== index,
+
+                'focus-effect bg-black text-white': focusedInputIndex === index,
+                'bg-zinc-100 ': index % 2 === 0 && !isInputFocused,
+                'bg-black text-white hover:bg-black':
+                    isInputFocused && focusedInputIndex === index,
+            }"
+            @click.stop="handleRowClick(index, item.student.id)"
+        >
+            <div
+                class="p-3 underline-offset-2 hover:underline"
+                :class="{
+                    'text-xs font-light':
+                        isInputFocused && focusedInputIndex !== index,
+                    ' text-sm font-semibold': focusedInputIndex === index,
+                }"
+            >
+                <span class="font-semibold"> {{ index + 1 }}.</span>
+                <span>
+                    {{ item.student.user.name }}
+                    {{ item.student.id }}
+                </span>
+            </div>
+
+            <div class="flex items-center justify-evenly text-xl font-semibold">
+                <input
+                    :ref="(el) => (inputRefs[index] = el)"
+                    v-model.number="item.point"
+                    :max="assessment.maximum_point"
+                    type="number"
+                    class="mr-2 w-16 rounded-md border-none bg-white text-xs text-black transition-transform focus:ring-black"
+                    :class="{
+                        'bg-zinc-100 ': index % 2 === 0,
+                    }"
+                    :placeholder="assessment.maximum_point"
+                    @focusin="handleFocusIn(index)"
+                    @focusout="handleFocusOut()"
+                    @keydown="onKeyDown($event, index, item)"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import { nextTick, reactive, ref } from "vue";
+import { usePage } from "@inertiajs/vue3";
+
+const assessment = usePage().props.assessment;
+const focusedInputIndex = ref(null);
+const points = reactive([]);
+
+const emit = defineEmits(["click", "updatePoints"]);
+const onInputChange = (studentId, point) => {
+    // Find index of the object where student_id matches
+    const index = points.findIndex((item) => item.student_id === studentId);
+
+    // If a match is found, update that item's point
+    if (index !== -1) {
+        points[index].point = point;
+    }
+    // If no match is found, add a new item to the array
+    else {
+        points.push({ student_id: studentId, point: point });
+    }
+    emit("updatePoints", points);
+};
+
+const inputRefs = reactive([]);
+const onKeyDown = (event, index, item) => {
+    switch (event.key) {
+        case "Enter":
+        case "Tab":
+        case "ArrowDown":
+            event.preventDefault();
+            moveFocus(index, "down");
+            nextTick(() => inputRefs[focusedInputIndex.value].focus());
+            break;
+        case "ArrowUp":
+            event.preventDefault();
+            moveFocus(index, "up");
+            nextTick(() => inputRefs[focusedInputIndex.value].focus());
+            break;
+        default:
+            break;
+    }
+
+    // Call onInputChange when the value changes
+    onInputChange(item.student.id, item.point);
+};
+const moveFocus = (currentIndex, direction) => {
+    if (direction === "down" && currentIndex < assessment.students.length - 1) {
+        focusedInputIndex.value = currentIndex + 1;
+    } else if (direction === "up" && currentIndex > 0) {
+        focusedInputIndex.value = currentIndex - 1;
+    }
+};
+const isInputFocused = ref(false);
+
+const handleFocusIn = (index) => {
+    focusedInputIndex.value = index;
+    isInputFocused.value = true;
+};
+
+const handleFocusOut = () => {
+    isInputFocused.value = false;
+    focusedInputIndex.value = null;
+};
+const handleRowClick = (index, studentId) => {
+    focusedInputIndex.value = index;
+    nextTick(() => inputRefs[focusedInputIndex.value].focus());
+    emit("click", studentId);
+};
+</script>
+<style scoped>
+.blur-effect *:not(.focus-effect) {
+    filter: blur(1.1px);
+}
+
+input:focus {
+    transform: scale(1.2);
+    z-index: 10;
+}
+
+.focus-effect {
+    z-index: 10;
+}
+</style>
