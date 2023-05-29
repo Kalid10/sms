ace89d0ec9185ffa3604c3617bb96a201e8fd703
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-05-29 18:46:30 +0300

    Add selected filters view

---
+++ resources/js/Views/Teacher/Assessments/Table/Filters/SelectedFilters.vue
@@ -0,0 +1,98 @@
+<template>
+    <div
+        class="flex w-full flex-wrap space-x-2"
+        :class="showFilter ? 'blur-sm' : 'blur-none'"
+    >
+        <FilterItem
+            v-if="search"
+            :value="search"
+            class-style="bg-purple-500 text-white"
+            icon-style="text-fuchsia-200 hover:text-black"
+            @remove="$emit('removeSearch')"
+        />
+        <FilterItem
+            v-if="selectedBatchSubjectName"
+            :value="selectedBatchSubjectName"
+            class-style="bg-emerald-400"
+            icon-style="text-black"
+            @remove="$emit('removeBatchSubject')"
+        />
+        <FilterItem
+            v-if="selectedAssessmentTypeName"
+            :value="selectedAssessmentTypeName"
+            @remove="$emit('removeAssessmentType')"
+        />
+        <FilterItem
+            v-if="selectedSchoolYear"
+            :value="selectedSchoolYear"
+            class-style="bg-cyan-400"
+            icon-style="text-black"
+            @remove="$emit('removeSchoolYear')"
+        />
+        <FilterItem
+            v-if="selectedSemester"
+            :value="selectedSemester"
+            class-style="bg-red-500 text-white"
+            icon-style="text-gray-200"
+            @remove="$emit('removeSemester')"
+        />
+        <FilterItem
+            v-if="selectedQuarter"
+            :value="selectedQuarter"
+            class-style="bg-neutral-700 text-white"
+            icon-style="text-gray-200"
+            @remove="$emit('removeQuarter')"
+        />
+        <PlusIcon
+            v-if="!showFilter"
+            class="hover:w-4.5 w-4 cursor-pointer hover:text-black"
+            @click="$emit('show')"
+        />
+    </div>
+</template>
+<script setup>
+import FilterItem from "@/Views/Teacher/Assessments/Table/Filters/FilterItem.vue";
+import { PlusIcon } from "@heroicons/vue/20/solid";
+
+defineEmits([
+    "show",
+    "removeQuarter",
+    "removeSemester",
+    "removeSchoolYear",
+    "removeAssessmentType",
+    "removeBatchSubject",
+    "removeSearch",
+]);
+
+const props = defineProps({
+    showFilter: {
+        type: Boolean,
+        required: true,
+    },
+    selectedAssessmentTypeName: {
+        type: String,
+        default: null,
+    },
+    selectedBatchSubjectName: {
+        type: String,
+        default: null,
+    },
+    selectedSchoolYear: {
+        type: String,
+        default: null,
+    },
+    selectedSemester: {
+        type: String,
+        default: null,
+    },
+    selectedQuarter: {
+        type: String,
+        default: null,
+    },
+    search: {
+        type: String,
+        default: null,
+    },
+});
+</script>
+<style scoped></style>
