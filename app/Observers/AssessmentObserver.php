9a2310fb18f9f655bc1fb0ff854442e27825b95e
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-06-01 00:51:36 +0300

    Update assessment observer to check if there are deleted students and restore before creating a new ones

--- app/Observers/AssessmentObserver.php
+++ app/Observers/AssessmentObserver.php
@@ -9,12 +9,13 @@ class AssessmentObserver
 {
     public function updated(Assessment $assessment): void
     {
-        if ($assessment->isDirty('status') && $assessment->status === Assessment::STATUS_SCHEDULED) {
+        if ($assessment->isDirty('status') && $assessment->status === Assessment::STATUS_SCHEDULED
+            || $assessment->status === Assessment::STATUS_PUBLISHED) {
             $this->createStudentAssessments($assessment);
         }
 
         if ($assessment->isDirty('status') && $assessment->status === Assessment::STATUS_DRAFT) {
-            $studentAssessments = StudentAssessment::where('assessment_id', $assessment->id)->whereNotNull('points')->get();
+            $studentAssessments = StudentAssessment::where('assessment_id', $assessment->id)->whereNotNull('point')->get();
             if ($studentAssessments->count() > 0) {
                 return;
             }
@@ -31,16 +32,28 @@ class AssessmentObserver
 
     private function createStudentAssessments(Assessment $assessment): void
     {
-        if ($assessment->status !== Assessment::STATUS_PUBLISHED) {
+        if ($assessment->status !== Assessment::STATUS_PUBLISHED && $assessment->status !== Assessment::STATUS_SCHEDULED) {
             return;
         }
 
         // Fetch all students in the batch and create student assessments for each student
         $assessment->batchSubject->students->each(function ($student) use ($assessment) {
-            StudentAssessment::create([
+            // Check if there is a soft-deleted record for the student assessment
+            $studentAssessment = StudentAssessment::withTrashed()->where([
                 'assessment_id' => $assessment->id,
                 'student_id' => $student->id,
-            ]);
+            ])->first();
+
+            // If there is a soft-deleted record, restore it
+            if ($studentAssessment && $studentAssessment->trashed()) {
+                $studentAssessment->restore();
+            } // If there is no record, create a new one
+            elseif (! $studentAssessment) {
+                StudentAssessment::create([
+                    'assessment_id' => $assessment->id,
+                    'student_id' => $student->id,
+                ]);
+            }
         });
     }
 }
