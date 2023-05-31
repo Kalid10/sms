4772fa19aaf1cfeade5dab8c40549ade55d1ac2d
Biniyam Lemma <lemmasbini@gmail.com>
2023-05-31 12:13:59 +0300

    Fix pull requests #296 review comments and add `AssessmentRequest`

--- app/Http/Controllers/API/AssessmentController.php
+++ app/Http/Controllers/API/AssessmentController.php
@@ -2,44 +2,24 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Requests\API\AssessmentRequest;
 use App\Http\Resources\AssessmentResource;
+use App\Models\Student;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Auth;
 
 class AssessmentController extends Controller
 {
-    public function index(): JsonResponse|Application|ResponseFactory|Response
+    public function index(AssessmentRequest $request, ?Student $student): AssessmentResource|Application|ResponseFactory|\Illuminate\Foundation\Application|Response
     {
-        $children = Auth::user()
-            ->load('guardian.children.user', 'guardian.children.assessments')
-            ->guardian->children;
-
-        return response(AssessmentResource::collection($children));
-    }
-
-    public function childAssessment($id): JsonResponse|Application|ResponseFactory|Response
-    {
-        // Find the authenticated user (guardian)
-        $guardian = Auth::user();
-        if (! $guardian) {
-            return response()->json([
-                'message' => 'Guardian not found.',
-            ], 404);
-        }
-
-        // Find the child under this guardian
-        $child = $guardian->guardian->children()->with('user', 'assessments')->find($id);
-
-        if (! $child) {
-            return response()->json([
-                'message' => 'Child not found.',
-            ], 404);
-        }
-
-        return response(new AssessmentResource($child));
+        return $student->exists ?
+            new AssessmentResource($student->load('user', 'assessments')) :
+            response(AssessmentResource::collection(
+                request()->user()
+                    ->load('guardian.children.user', 'guardian.children.assessments')
+                    ->guardian->children
+            ));
     }
 }
--- routes/api/assessments.php
+++ routes/api/assessments.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\API\AssessmentController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('assessments/')->middleware('auth:sanctum')->name('assessments.')->group(function () {
-    Route::get('', [AssessmentController::class, 'index'])->name('index');
-    Route::get('{id}', [AssessmentController::class, 'childAssessment'])->name('child');
+Route::controller(AssessmentController::class)->prefix('assessments/')->middleware('auth:sanctum')->name('assessments.')->group(function () {
+    Route::get('{student?}', 'index')->name('index');
 });
