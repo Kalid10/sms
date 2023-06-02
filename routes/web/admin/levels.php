62a1e1180f8e964b587cef0a22e68ef21f892abc
Biniyam Lemma <lemmasbini@gmail.com>
2023-06-02 19:09:27 +0300

    - Fix some design errors on the `Levels` single view page, `Schedule`, `Login`, and `Profile` pages - Move students, levels, and subjects views route to `admin` routes

--- routes/web/admin/levels.php
+++ routes/web/admin/levels.php
@@ -5,6 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(LevelController::class)->prefix('levels/')->middleware(['checkUserRole:manage-levels'])->name('levels.')->group(function () {
     Route::post('create', 'create')->name('create');
-    Route::get('', 'list')->name('list');
-    Route::get('/{level}', 'show')->name('show');
 });
--- routes/web/admin/subjects.php
+++ routes/web/admin/subjects.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\Web\SubjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(SubjectController::class)->prefix('subjects/')->middleware(['checkUserRole:manage-subjects'])->name('subjects.')->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/{subject}', 'show')->name('show');
     Route::post('create', 'create')->name('create');
     Route::post('create-bulk', 'createBulk')->name('create-bulk');
     Route::post('update', 'update')->name('update');
