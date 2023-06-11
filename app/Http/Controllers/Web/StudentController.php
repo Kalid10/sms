f800f8ca4281e60dadb8e0dadbcf7ceaba775c28
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-06-12 02:45:31 +0300

    Remove 'show' function from StudentController.php: Shifted to a new approach for loading student detail page for admin users

--- app/Http/Controllers/Web/StudentController.php
+++ app/Http/Controllers/Web/StudentController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Web;
 
 use App\Models\Batch;
 use App\Models\BatchStudent;
-use App\Models\Level;
 use App\Models\SchoolYear;
 use App\Models\Student;
+use App\Services\StudentService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -16,21 +16,7 @@ class StudentController extends Controller
 {
     public function index(Request $request): Response
     {
-        $searchKey = $request->input('search');
-
-        // Get per page value
-        $perPage = $request->input('per_page', 15);
-
-        $students = Student::with([
-            'user:id,name,email,phone_number,gender',
-            'batches.batch:id,section',
-            'batches.level',
-        ])->select('id', 'user_id')
-            ->when($searchKey, function ($query) use ($searchKey) {
-                return $query->whereHas('user', function ($query) use ($searchKey) {
-                    return $query->where('name', 'like', "%{$searchKey}%");
-                });
-            })->paginate($perPage);
+        $students = StudentService::getAllStudents($request);
 
         // Get all batches
         $batches = Batch::where('school_year_id', SchoolYear::getActiveSchoolYear()->id)
@@ -54,34 +40,6 @@ class StudentController extends Controller
             'batches' => $batches,
             'selected_batch' => $selectedBatch,
             'batch_students' => $batchStudents,
-        ]);
-    }
-
-    public function show(Student $student): Response
-    {
-        $student = $student->load(
-            'user',
-            'guardian',
-            'guardian.user',
-            'batches',
-            'batches.batch',
-            'batches.batch.level',
-        );
-
-        return Inertia::render('Admin/Students/Single', [
-            'student' => $student,
-            'schedule' => $student->activeBatch()->load(
-                'schedule:id,school_period_id,batch_subject_id,day_of_week,batch_id',
-                'schedule.batchSubject:id,teacher_id,subject_id,weekly_frequency',
-                'schedule.batchSubject.subject',
-                'schedule.schoolPeriod:id,name,start_time,duration,is_custom,level_category_id'
-            )->only('schedule')['schedule'],
-            'active_batch' => $student->activeBatch(load: ['level', 'level.levelCategory']),
-            'attendance' => [
-                'absence_rate' => $student->absenteePercentage(),
-                'absentee_records' => $student->absenteeRecords()->get(),
-            ],
-            'periods' => Level::find($student->activeBatch()->level->id)->levelCategory->schoolPeriods,
         ]);
     }
 
