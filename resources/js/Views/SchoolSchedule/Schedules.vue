aadbc749dd31df1d9978a790f36610ba780cc771
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-06-13 22:52:39 +0300

    Update schedule view style

--- resources/js/Views/SchoolSchedule/Schedules.vue
+++ resources/js/Views/SchoolSchedule/Schedules.vue
@@ -9,7 +9,10 @@
                 class="my-1 flex w-full items-center justify-center space-x-2 rounded-lg py-3 font-medium"
                 :class="index % 2 === 1 ? 'bg-gray-50' : 'bg-white'"
             >
-                <div class="text-xs 2xl:text-xs">- {{ item.title }}</div>
+                <CalendarDaysIcon class="w-4 text-zinc-500" />
+                <div class="text-xs text-zinc-500 2xl:text-sm">
+                    {{ item.title }}
+                </div>
             </div>
 
             <EmptyView
@@ -17,14 +20,19 @@
                 title="No Schedule found for today!"
             />
         </div>
-        <LinkCell href="/school-schedules" value="View All Schedules" />
+        <!--        <LinkCell href="/school-schedules" value="View All Schedules" />-->
+        <SecondaryButton
+            title="Add Event"
+            class="w-2/3 !rounded-2xl bg-zinc-700 text-white"
+        />
     </div>
 </template>
 <script setup>
 import { computed } from "vue";
 import { usePage } from "@inertiajs/vue3";
 import EmptyView from "@/Views/EmptyView.vue";
-import LinkCell from "@/Components/LinkCell.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import { CalendarDaysIcon } from "@heroicons/vue/24/solid";
 
 const schoolSchedule = computed(() => usePage().props.school_schedule);
 </script>
