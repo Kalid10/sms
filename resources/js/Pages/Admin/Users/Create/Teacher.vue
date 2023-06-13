16a1e7d332cbf4c7cfbd84f1c8d435ac8aa79ec5
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-06-13 22:41:44 +0300

    Update registration pages style

--- resources/js/Pages/Admin/Users/Create/Teacher.vue
+++ resources/js/Pages/Admin/Users/Create/Teacher.vue
@@ -1,51 +1,74 @@
 <template>
-    <div class="grid-rows-12 grid sm:grid-cols-12 md:w-full">
+    <div class="grid-rows-12 grid h-screen p-6 sm:grid-cols-12 md:w-full">
         <div class="col-span-3 mb-6 flex shrink-0 flex-col md:mb-0">
-            <Heading
-                value="Register new teacher"/>
+            <Heading value="Register new teacher" />
             <Heading
                 value="Fill in the information required."
-                size="sm" class="text-xs font-normal text-gray-500"/>
+                size="sm"
+                class="text-xs !font-light text-gray-500"
+            />
         </div>
         <div class="col-span-8">
             <div class="w-full max-w-4xl rounded-lg bg-white">
-
-                <TeacherFormElement @submit="submit"
-                >
+                <TeacherFormElement title="Register Teacher" @submit="submit">
                     <TeacherTextInput
-                        v-model="form.name" label="Name" placeholder="Full name of new teacher"
-                        :error="form.errors.name" required/>
+                        v-model="form.name"
+                        label="Name"
+                        placeholder="Full name of new teacher"
+                        :error="form.errors.name"
+                        required
+                    />
 
                     <div class="flex gap-3">
                         <TeacherTextInput
-                            v-model="form.username" class="w-full" label="User Name" :error="form.errors.username"
-                            placeholder="username" required/>
+                            v-model="form.username"
+                            class="w-full"
+                            label="User Name"
+                            :error="form.errors.username"
+                            placeholder="username"
+                            required
+                        />
 
                         <TeacherTextInput
-                            v-model="form.phone_number" class="w-full" label="Phone number" placeholder="phone number"
-                            :error="form.errors.phone_number" required/>
+                            v-model="form.phone_number"
+                            class="w-full"
+                            label="Phone number"
+                            placeholder="phone number"
+                            :error="form.errors.phone_number"
+                            required
+                        />
                     </div>
 
                     <div class="flex gap-3">
                         <TeacherTextInput
-                            v-model="form.email" class="w-full" label="Email" type="email" placeholder="email"
-                            :error="form.errors.email" required/>
+                            v-model="form.email"
+                            class="w-full"
+                            label="Email"
+                            type="email"
+                            placeholder="email"
+                            :error="form.errors.email"
+                            required
+                        />
                         <TeacherTextInput
-                            v-model="form.gender" class="w-full" label="Gender" placeholder="gender"
-                            :error="form.errors.gender" required/>
+                            v-model="form.gender"
+                            class="w-full"
+                            label="Gender"
+                            placeholder="gender"
+                            :error="form.errors.gender"
+                            required
+                        />
                     </div>
                 </TeacherFormElement>
             </div>
         </div>
     </div>
-
 </template>
 
 <script setup>
 import TeacherFormElement from "@/Components/FormElement.vue";
 import TeacherTextInput from "@/Components/TextInput.vue";
 import Heading from "@/Components/Heading.vue";
-import {useForm} from "@inertiajs/vue3";
+import { useForm } from "@inertiajs/vue3";
 
 const form = useForm({
     name: "",
@@ -57,6 +80,6 @@ const form = useForm({
 });
 
 const submit = () => {
-    form.post(route('register.admin'));
-}
+    form.post(route("register.admin"));
+};
 </script>
