293851911dc8d194364197a64e150c40b1679f46
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-06-16 11:35:17 +0300

    Update stats component

--- resources/js/Views/Teacher/Views/Batches/BatchPerformance/Index.vue
+++ resources/js/Views/Teacher/Views/Batches/BatchPerformance/Index.vue
@@ -1,27 +1,27 @@
 <template>
     <div
-        class="flex w-full justify-between divide-x divide-gray-500 rounded-md border-2 border-zinc-700 py-4"
+        class="flex w-full justify-between divide-x divide-gray-200 rounded-md bg-gray-50 py-4 shadow-sm"
     >
         <Item
-            title="Average"
+            title="Class Average"
             :value="grade?.score"
             value-style="text-negative-100"
             :icon="ArrowSmallDownIcon"
         />
         <Item
-            title="Rank"
+            title="Class Rank"
             :value="grade?.rank"
             value-style="text-positive-100"
             :icon="ArrowSmallUpIcon"
         />
         <Item
-            title="Attendance"
+            title="Class Attendance"
             :value="grade?.attendance ? grade?.attendance + '%' : '-'"
             value-style="text-positive-100"
             :icon="ArrowSmallUpIcon"
         />
         <Item
-            title="Conduct"
+            title="Class Conduct"
             :value="grade?.conduct"
             value-style="text-negative-100"
             :icon="ArrowSmallDownIcon"
