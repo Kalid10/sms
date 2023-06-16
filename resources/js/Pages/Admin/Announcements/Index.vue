2028d24b51f681161697f2f7fb6f21206a936f1e
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-06-16 14:28:32 +0300

    Update announcement views for admin and teacher

--- resources/js/Pages/Admin/Announcements/Index.vue
+++ resources/js/Pages/Admin/Announcements/Index.vue
@@ -1,35 +1,36 @@
 <template>
-    <div class="flex min-h-screen w-full flex-col bg-gray-50/50 px-10">
-        <Title class="w-full" title="Announcements" />
-        <div
-            class="flex w-full justify-between divide-gray-100 rounded-lg py-6 pr-5"
-        >
-            <div class="flex h-full w-5/12 flex-col space-y-6 p-3">
-                <SelectedAnnouncementView
-                    class="h-2/5"
-                    :selected-announcement="selectedAnnouncement"
-                />
-                <SelectedAnnouncementView
-                    class="h-2/5"
-                    :selected-announcement="selectedAnnouncement2"
-                />
+    <div class="flex min-h-screen w-full flex-col bg-gray-50/40 px-10 py-5">
+        <div class="flex w-full flex-col items-center space-y-2 px-5 py-3">
+            <div class="flex w-full items-center justify-between pr-5">
+                <Title class="w-5/12" title="Announcements" />
+                <TextInput placeholder="Search Announcements" class="w-5/12" />
             </div>
-            <div class="w-6/12">
-                <Announcements
-                    :show-header="false"
-                    class-style="bg-gray-50/50 rounded-lg py-3"
-                />
+
+            <div class="flex w-full justify-between divide-gray-100 py-6 pr-5">
+                <div class="flex h-full w-5/12 flex-col space-y-6 p-3">
+                    <SelectedAnnouncementView
+                        class="h-2/5"
+                        :selected-announcement="selectedAnnouncement"
+                    />
+                    <SelectedAnnouncementView
+                        class="h-2/5"
+                        :selected-announcement="selectedAnnouncement2"
+                    />
+                </div>
+                <div class="w-6/12">
+                    <Announcements :show-header="false" class-style="d" />
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script setup>
 import Announcements from "@/Views/Announcements/Index.vue";
-
 import { computed, ref } from "vue";
 import { usePage } from "@inertiajs/vue3";
 import Title from "@/Views/Teacher/Views/Title.vue";
 import SelectedAnnouncementView from "@/Views/Announcements/SelectedAnnouncement.vue";
+import TextInput from "@/Components/TextInput.vue";
 
 const announcements = computed(() => usePage().props.announcements);
 const selectedAnnouncement = ref(announcements.value?.data[0]);
