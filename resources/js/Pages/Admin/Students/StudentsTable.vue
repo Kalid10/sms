1159cf93e8dec7b399836764f28a01c845d30557
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-06-19 18:01:11 +0300

    Update student absentee stat view

--- resources/js/Pages/Admin/Students/StudentsTable.vue
+++ resources/js/Pages/Admin/Students/StudentsTable.vue
@@ -166,11 +166,18 @@
                 <div
                     v-for="(absentee, index) in todayAbsentees"
                     :key="index"
-                    class="flex w-full flex-col justify-start"
+                    class="flex w-full flex-col justify-start rounded-lg py-2"
+                    :class="index % 2 === 0 ? 'bg-gray-50' : ''"
                 >
-                    <span class="font-semibold">
-                        Name: {{ absentee.user.name }} ({{ absentee.reason }}),
-                    </span>
+                    <div class="flex w-full px-4 text-sm font-medium">
+                        <div class="">
+                            {{ index + 1 }}.
+                            {{ absentee.user.name }}
+                        </div>
+                        <div v-if="absentee?.reason">
+                            ({{ absentee.reason }}),
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -188,9 +195,15 @@
                     :key="index"
                     class="flex w-full flex-col justify-start"
                 >
-                    <span class="font-semibold">
-                        Name: {{ absentee.user.name }} ({{ absentee.reason }}),
-                    </span>
+                    <div class="flex w-full px-4 text-sm font-medium">
+                        <div class="">
+                            {{ index + 1 }}.
+                            {{ absentee.user.name }}
+                        </div>
+                        <div v-if="absentee?.reason">
+                            ({{ absentee.reason }}),
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -216,7 +229,7 @@ import Title from "@/Views/Teacher/Views/Title.vue";
 const props = defineProps({
     url: {
         type: String,
-        default: "/admin/students/",
+        default: "/admin/students",
     },
     showTitle: {
         type: Boolean,
@@ -332,7 +345,7 @@ const config = [
     {
         name: "Name",
         key: "name",
-        link: "/students/{id}",
+        link: "/admin/teachers/students/{id}",
         align: "left",
         type: "custom",
     },
