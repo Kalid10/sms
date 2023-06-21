087ab72a3efc8af2ca718e89eed1cd430244e3aa
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-06-21 19:54:43 +0300

    Add absentee stat view for admin

---
+++ resources/js/Views/Admin/Absentee/AbsenteeStats.vue
@@ -0,0 +1,22 @@
+<template>
+    <div
+        class="flex flex-col space-y-3 divide-y divide-gray-600 rounded-lg bg-zinc-800 p-3 text-white shadow-sm"
+    >
+        <div class="flex items-center space-x-2.5 pl-3">
+            <UserMinusIcon class="w-6" />
+            <div class="text-sm font-medium">Today's Absentees</div>
+        </div>
+        <div
+            class="flex w-full items-center justify-between divide-x divide-gray-600 pt-4"
+        >
+            <AbsenteeStatItem class="w-1/3" value="4" title="Students" />
+            <AbsenteeStatItem class="w-1/3" value="2" title="Teachers" />
+            <AbsenteeStatItem class="w-1/3" value="1" title="Admins" />
+        </div>
+    </div>
+</template>
+<script setup>
+import AbsenteeStatItem from "@/Views/Admin/Absentee/AbsenteeStatItem.vue";
+import { UserMinusIcon } from "@heroicons/vue/24/outline";
+</script>
+<style scoped></style>
