93ddf6f5e8763298c96081abfe2d888cc3a4781e
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-06-25 17:14:59 +0300

    Shifted primary lesson plan data fetching functionality to Teacher's Service

--- app/Http/Controllers/Web/LessonPlanController.php
+++ app/Http/Controllers/Web/LessonPlanController.php
@@ -3,19 +3,13 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Requests\LessonPlans\UpdateOrCreateRequest;
-use App\Models\Batch;
 use App\Models\BatchSession;
 use App\Models\BatchSubject;
 use App\Models\LessonPlan;
-use App\Models\Quarter;
-use App\Models\SchoolYear;
-use App\Models\Semester;
-use App\Models\Teacher;
 use App\Models\User;
 use App\Services\OpenAIService;
-use Carbon\Carbon;
+use App\Services\TeacherService;
 use Exception;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -24,7 +18,7 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class LessonPlanController extends Controller
 {
-    public function index(Request $request, OpenAIService $openAIService): Response|\Illuminate\Http\RedirectResponse
+    public function index(Request $request, OpenAIService $openAIService, TeacherService $teacherService): Response|\Illuminate\Http\RedirectResponse
     {
         $request->validate([
             'batch_subject_id' => 'nullable|exists:batch_subjects,id',
@@ -37,75 +31,6 @@ class LessonPlanController extends Controller
         if (! $teacherId) {
             abort(403);
         }
-        $batchSubjectId = $request->input('batch_subject_id') ?? BatchSubject::where('teacher_id', $teacherId)
-            ->whereHas('batch', function ($query) {
-                $query->where('school_year_id', SchoolYear::getActiveSchoolYear()->id);
-            })->first()?->id;
-        $batchSubject = BatchSubject::with([
-            'subject:id,full_name',
-            'batch:id,section,level_id',
-            'batch.level:id,name',
-        ])->where([
-            ['id', $batchSubjectId],
-            ['teacher_id', $teacherId],
-        ])->firstOrFail();
-
-        if ($batchSubject->teacher_id !== $teacherId && ! auth()->user()->type === User::TYPE_ADMIN) {
-            return redirect()->back()->with('error', 'You are not assigned to this subject.');
-        }
-
-        $batchId = $batchSubject->batch_id;
-        $currentMonth = $request->input('month') ? Carbon::createFromFormat('Y-m', $request->input('month')) : Carbon::now();
-        $firstDayOfMonth = $currentMonth->copy()->startOfMonth();
-        $lastDayOfMonth = $currentMonth->copy()->endOfMonth();
-
-        $teacherSubjects = BatchSubject::with([
-            'subject:id,full_name',
-            'batch:id,section,level_id',
-            'batch.level:id,name,level_category_id',
-        ])->where('teacher_id', $teacherId)
-            ->whereHas('batch', fn ($query) => $query->where('school_year_id', SchoolYear::getActiveSchoolYear()->id))
-            ->distinct()
-            ->get(['id', 'subject_id', 'batch_id']);
-
-        $quarterFilter = $request->input('quarter_id');
-
-        $batchSessionsWithLessonPlans = BatchSession::where('teacher_id', $teacherId)
-            ->whereHas('batchSchedule.batchSubject', fn ($query) => $query->where('batch_subject_id', $batchSubject->id))
-            ->whereBetween('date', [$firstDayOfMonth, $lastDayOfMonth])
-            ->when($quarterFilter, function (Builder $query) use ($quarterFilter) {
-                $query->whereHas('batchSchedule', function (Builder $query) use ($quarterFilter) {
-                    $query->where('quarter_id', $quarterFilter);
-                });
-            })
-            ->with([
-                'batchSchedule.schoolPeriod',
-                'batchSchedule.batchSubject.subject',
-                'lessonPlan',
-            ])
-            ->get()
-            ->sortBy('date')
-            ->groupBy(function ($batchSession) use ($firstDayOfMonth) {
-                $date = Carbon::parse($batchSession->date);
-                $weekNumber = $date->weekOfYear - $firstDayOfMonth->weekOfYear + 1;
-
-                return 'week'.max($weekNumber, 1);
-            });
-
-        $weeksInMonth = (int) ($currentMonth->daysInMonth / 7) + ($currentMonth->daysInMonth % 7 != 0 ? 1 : 0);
-
-        $weeklyBatchSessions = array_fill_keys(array_map(fn ($i) => "week{$i}", range(1, $weeksInMonth)), collect([]));
-
-        foreach ($batchSessionsWithLessonPlans as $weekKey => $batchSessions) {
-            $weeklyBatchSessions[$weekKey] = $batchSessions;
-        }
-
-        $months = collect(range(1, 12))->map(function ($month) {
-            return [
-                'value' => Carbon::today()->month($month)->format('Y-m'),
-                'label' => Carbon::today()->month($month)->format('F'),
-            ];
-        });
 
         $prompt = $request->input('prompt') ?? null;
         $page = match (auth()->user()->type) {
@@ -114,24 +39,11 @@ class LessonPlanController extends Controller
             default => throw new Exception('Type unknown!'),
         };
 
-        $quarters = Quarter::with('semester.schoolYear')->get();
-        $semesters = Semester::with('schoolYear')->get();
-        $schoolYears = SchoolYear::all();
+        $lessonPlanData = $teacherService->getLessonPlansData($request, $teacherId);
 
-        return Inertia::render($page, [
-            'quarters' => $quarters,
-            'semesters' => $semesters,
-            'school_years' => $schoolYears,
-            'batch_sessions' => $weeklyBatchSessions,
-            'batch' => Batch::find($batchId)->load('level'),
-            'subjects' => $teacherSubjects,
-            'weeks_in_month' => $weeksInMonth,
-            'lesson_plan_subject' => $batchSubject,
-            'months' => $months,
-            'selected_month' => $currentMonth->format('Y-m'),
-            'teacher' => Teacher::find($teacherId)->load('user'),
-            'questions' => $prompt ? Inertia::lazy(fn () => $openAIService->lessonPlanHelper($prompt, $batchSubject)) : null,
-        ]);
+        return Inertia::render($page, array_merge($lessonPlanData, [
+            'questions' => $prompt ? Inertia::lazy(fn () => $openAIService->lessonPlanHelper($prompt, $lessonPlanData['lesson_plan_subject'])) : null,
+        ]));
     }
 
     public function updateOrCreate(UpdateOrCreateRequest $request): \Illuminate\Http\RedirectResponse
