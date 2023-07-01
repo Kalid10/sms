1ca524ea840e163a3b230e685378a9ed81a281a3
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-07-01 20:17:38 +0300

    Update attendance card component

--- resources/js/Views/Teacher/Views/Home/AttendanceCard.vue
+++ resources/js/Views/Teacher/Views/Home/AttendanceCard.vue
@@ -2,7 +2,7 @@
 
 <template>
     <div
-        class="flex h-2/5 w-full flex-col items-center justify-center space-y-3 rounded-lg bg-green-300 text-center text-sm shadow-sm"
+        class="flex h-28 w-full flex-col items-center justify-center space-y-3 rounded-lg bg-green-300 text-center text-sm shadow-sm"
     >
         <div class="text-center text-5xl font-bold">10%</div>
 
