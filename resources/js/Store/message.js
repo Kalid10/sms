f4482164eb5af498a4b16c66a5e20871d7b0ebdb
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-07-04 18:14:50 +0300

    Add message store

---
+++ resources/js/Store/message.js
@@ -0,0 +1,132 @@
+import { defineStore } from "pinia";
+import axiosClient from "../https";
+import { usePage } from "@inertiajs/vue3";
+import { computed } from "vue";
+
+export const useMessageStore = defineStore({
+    id: "message",
+    state: () => ({
+        isLoading: false,
+        auth_id: computed(() => usePage().props.auth.user.id),
+        messages: {},
+        contacts: {},
+        token: null,
+        conversationStatus: false,
+        records: {},
+        activeChat: {
+            id: null,
+            name: null,
+            avatar: null,
+            active_status: null,
+        },
+    }),
+    getters: {},
+    actions: {
+        async authenticate() {
+            try {
+                const response = await axiosClient.post("/chat/api/chat/auth", {
+                    socket_id: "15886.48502",
+                    channel_name: "private-chatify",
+                });
+                console.log(response.data);
+                this.token = response.data;
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async fetchMessages(from) {
+            try {
+                this.isLoading = true;
+                const response = await axiosClient.post(
+                    "/chat/api/fetchMessages",
+                    {
+                        id: from,
+                    }
+                );
+                this.messages = response.data;
+                this.isLoading = false;
+                return response.data;
+            } catch (error) {
+                console.error(error);
+
+                this.isLoading = false;
+            }
+        },
+
+        async getContacts() {
+            try {
+                this.isLoading = true;
+                const { data } = await axiosClient.get("/chat/api/getContacts");
+                this.contacts = {};
+                this.contacts = data.contacts;
+
+                this.isLoading = false;
+            } catch (error) {
+                console.error(error);
+
+                this.isLoading = false;
+            }
+        },
+
+        async sendMessage(message) {
+            try {
+                const { data } = await axiosClient.post(
+                    "/chat/api/sendMessage",
+                    message
+                );
+                const updatedMessage = {
+                    ...data.message,
+                    body: data.message.message,
+                };
+                delete updatedMessage.message;
+                this.messages.messages.unshift(updatedMessage);
+            } catch (error) {
+                console.error(error);
+            }
+        },
+
+        async markAsRead(id) {
+            this.isLoading = true;
+            try {
+                const { data } = await axiosClient.post("/chat/api/makeSeen", {
+                    id: id,
+                });
+
+                this.isLoading = false;
+            } catch (error) {
+                console.error(error);
+
+                this.isLoading = false;
+            }
+        },
+
+        async searchContacts(query) {
+            this.isLoading = true;
+            try {
+                const { data } = await axiosClient.get(
+                    `/chat/api/search?input=${query}`
+                );
+                this.records = data.records;
+
+                this.isLoading = false;
+            } catch (error) {
+                console.error(error);
+
+                this.isLoading = false;
+            }
+        },
+        async setActiveStatus(status) {
+            try {
+                const { data } = await axiosClient.post(
+                    "/chat/api/setActiveStatus",
+                    { status: status }
+                );
+                this.activeChat.active_status = data.status;
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    },
+});
+
+export default useMessageStore;
