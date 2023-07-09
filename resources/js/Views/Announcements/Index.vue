b6a3e3551f721c5d855cf76697247bf112c373ae
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-07-09 20:29:19 +0300

    Separate show announcement to a separate view

--- resources/js/Views/Announcements/Index.vue
+++ resources/js/Views/Announcements/Index.vue
@@ -17,7 +17,6 @@
                 <SquaresPlusIcon
                     v-if="isAdmin() && view === 'admin'"
                     class="w-5 cursor-pointer text-zinc-700 hover:scale-105"
-                    @click="showAddAnnouncement = true"
                 />
             </div>
         </div>
@@ -56,56 +55,9 @@
         </div>
 
         <Modal v-model:view="showAnnouncement">
-            <div class="flex w-full flex-col space-y-5 rounded-lg bg-white p-5">
-                <div class="text-center text-2xl font-medium">
-                    {{ selectedAnnouncement.title }}
-                </div>
-
-                <div class="flex items-center text-sm font-light">
-                    {{ selectedAnnouncement.body }}
-                </div>
-
-                <div>
-                    <div class="py-2 text-sm font-medium">
-                        <span
-                            v-for="(
-                                target, index
-                            ) in selectedAnnouncement.target_group"
-                            :key="index"
-                            class="mr-2 rounded-md bg-zinc-700 px-3 py-1 text-xs font-medium uppercase text-white"
-                        >
-                            {{ target }}
-                        </span>
-                    </div>
-                    <div
-                        class="flex items-end justify-between space-y-2 text-xs"
-                    >
-                        <div>
-                            Expires
-                            {{
-                                moment(
-                                    selectedAnnouncement.expires_on
-                                ).fromNow()
-                            }}
-                        </div>
-                        <div>
-                            <div>
-                                Posted
-                                {{
-                                    moment(
-                                        selectedAnnouncement.created_at
-                                    ).fromNow()
-                                }}, By
-                                <span
-                                    class="cursor-pointer underline-offset-2 hover:scale-105 hover:font-semibold hover:underline"
-                                >
-                                    {{ selectedAnnouncement.author.user.name }}
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <ShowAnnouncementView
+                :selected-announcement="selectedAnnouncement"
+            />
         </Modal>
     </div>
 </template>
@@ -115,12 +67,12 @@ import { router, usePage } from "@inertiajs/vue3";
 import Item from "@/Views/Announcements/Item.vue";
 import LinkCell from "@/Components/LinkCell.vue";
 import Modal from "@/Components/Modal.vue";
-import moment from "moment/moment";
 import EmptyView from "@/Views/EmptyView.vue";
 import { debounce } from "lodash";
 import Pagination from "@/Components/Pagination.vue";
 import { isAdmin } from "@/utils";
 import { SquaresPlusIcon } from "@heroicons/vue/20/solid";
+import ShowAnnouncementView from "@/Views/Announcements/ShowAnnouncement.vue";
 
 const props = defineProps({
     url: {
