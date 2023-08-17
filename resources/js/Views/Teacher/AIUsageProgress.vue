dd5c90aabd34d33cdb88d5821db99ee3acc8f1dc
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-08-17 11:56:21 +0300

    Update AI usage progress component

--- resources/js/Views/Teacher/AIUsageProgress.vue
+++ resources/js/Views/Teacher/AIUsageProgress.vue
@@ -1,25 +1,45 @@
 <template>
-    <div>
-        <span class="font-semibold">
-            {{ $t("questionPreparation.dailyAIUsageProgress") }} ({{
-                openaiUsagePercentage
-            }})
-        </span>
-    </div>
-    <div class="w-full rounded-full bg-purple-200">
-        <div
-            :class="progressBarClass"
-            :style="{ width: openaiUsagePercentage }"
-        >
-            {{ openaiUsagePercentage }}
+    <div class="flex flex-col space-y-2">
+        <div class="text-center font-semibold uppercase">
+            {{ $t("questionPreparation.dailyAIUsageProgress") }}
+        </div>
+        <div class="w-full rounded-full bg-brand-100">
+            <div
+                :class="progressBarClass"
+                :style="{ width: openaiUsagePercentage }"
+            >
+                {{ openaiUsagePercentage }}
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
-import { usePage } from "@inertiajs/vue3";
-import { computed } from "vue";
+import { router, usePage } from "@inertiajs/vue3";
+import { computed, onMounted, watch } from "vue";
 
+const props = defineProps({
+    updateUsage: {
+        type: Boolean,
+        default: false,
+    },
+    page: {
+        type: String,
+        default: "/teacher/copilot",
+    },
+});
+
+onMounted(() => {
+    fetchAIUsage();
+});
+
+watch(
+    () => props.updateUsage,
+    () => {
+        fetchAIUsage();
+        props.updateUsage = false;
+    }
+);
 const openaiDailyUsage = computed(() => usePage().props.openai_daily_usage);
 const openaiDailyUsageLimit = computed(
     () => usePage().props.openai_daily_usage_limit
@@ -28,15 +48,31 @@ const openaiDailyUsageLimit = computed(
 const openaiUsagePercentage = computed(() => {
     const percentage =
         (openaiDailyUsage.value / openaiDailyUsageLimit.value) * 100;
-    return `${percentage}%`;
+    return `${Math.min(percentage, 100).toFixed(1)}%`;
 });
 
 const progressBarClass = computed(() => {
     const percentage = parseFloat(openaiUsagePercentage.value);
+    if (percentage < 20)
+        return "rounded-full bg-brand-100 p-0.5 text-center text-xs font-medium leading-none text-black";
     if (percentage < 50)
-        return "rounded-full bg-green-400 p-0.5 text-center text-xs font-medium leading-none text-white";
+        return "rounded-full bg-brand-250 p-0.5 text-center text-xs font-medium leading-none text-white";
     if (percentage < 75)
-        return "rounded-full bg-yellow-400 p-0.5 text-center text-xs font-medium leading-none text-white";
-    return "rounded-full bg-red-600 p-0.5 text-center text-xs font-medium leading-none text-white";
+        return "rounded-full bg-brand-300 p-0.5 text-center text-xs font-medium leading-none text-white";
+    return "rounded-full bg-brand-400 p-0.5 text-center text-xs font-medium leading-none text-white";
 });
+
+const fetchAIUsage = () => {
+    router.get(
+        props.page,
+        {},
+        {
+            preserveState: true,
+            only: ["openai_daily_usage", "openai_daily_usage_limit"],
+            onFinish: () => {
+                props.updateUsage = false;
+            },
+        }
+    );
+};
 </script>
