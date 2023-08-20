7936f90187831f8d21a799844ba259f53a6a670e
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-08-20 11:32:35 +0300

    Update school year status middleware

--- app/Http/Middleware/CheckSchoolYearStatus.php
+++ app/Http/Middleware/CheckSchoolYearStatus.php
@@ -14,12 +14,19 @@ class CheckSchoolYearStatus
      *
      * @param  Closure(Request): (Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, bool $isReady): Response
     {
         $activeSchoolYear = SchoolYear::getActiveSchoolYear();
 
-        if ($activeSchoolYear == null || ! $activeSchoolYear->is_ready) {
-            return redirect()->route('getting-started.index');
+        if ($isReady) {
+            if ($activeSchoolYear === null || ! $activeSchoolYear->is_ready) {
+                return redirect()->route('getting-started.index');
+            }
+        }
+
+        if (! $isReady && $activeSchoolYear->is_ready) {
+
+            return redirect()->route('admin.show');
         }
 
         return $next($request);
