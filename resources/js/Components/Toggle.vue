9fd59038f911deaa16396e3372209e72d7ea5a20
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-08-24 09:39:35 +0300

    Update theme

--- resources/js/Components/Toggle.vue
+++ resources/js/Components/Toggle.vue
@@ -16,7 +16,7 @@
                 @input="input"
             />
             <span
-                class="peer h-6 w-11 rounded-full bg-brand-150 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-black peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none"
+                class="peer h-6 w-11 rounded-full bg-brand-150 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-brand-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none"
             ></span>
         </label>
     </div>
