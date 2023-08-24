f7a921fb4c7f04a064bea9da2ab0889166a7c3d6
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-08-24 16:42:30 +0300

    Add the add button on pending checkouts table

--- resources/js/Views/Admin/Inventory/PendingInventoryItems.vue
+++ resources/js/Views/Admin/Inventory/PendingInventoryItems.vue
@@ -10,8 +10,20 @@
         class="!w-9/12 !rounded-lg p-4 shadow-sm"
     >
         <template #table-header>
-            <div class="w-full px-6 py-5 text-2xl font-semibold">
-                Pending CheckOuts
+            <div class="flex w-full justify-evenly py-5">
+                <div
+                    class="text-2xl font-semibold"
+                    :class="canManageInventory ? 'w-10/12' : 'w-full px-6'"
+                >
+                    Pending CheckOuts
+                </div>
+                <PrimaryButton
+                    v-if="canManageInventory"
+                    class="h-fit w-fit"
+                    @click="emit('add')"
+                >
+                    Add Item
+                </PrimaryButton>
             </div>
         </template>
 
@@ -49,7 +61,11 @@ import { computed } from "vue";
 import { usePage } from "@inertiajs/vue3";
 import TableElement from "@/Components/TableElement.vue";
 import Pagination from "@/Components/Pagination.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 
+const emit = defineEmits(["add"]);
+
+const canManageInventory = computed(() => usePage().props.can_manage_inventory);
 const pendingItems = computed(
     () => usePage().props.pending_inventory_check_outs
 );
