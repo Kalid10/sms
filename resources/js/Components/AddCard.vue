3dfefed19e3943779b8331b9d4b4b1cb2a463d43
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-10-11 16:09:03 +0300

    Add 'AddCard.vue' component

---
+++ resources/js/Components/AddCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <div
+        class="flex w-fit cursor-pointer items-center justify-center rounded-xl bg-brand-100 p-4 shadow-sm hover:scale-105"
+    >
+        <PlusCircleIcon class="w-12 text-brand-250" />
+    </div>
+</template>
+
+<script setup>
+import { PlusCircleIcon } from "@heroicons/vue/24/solid";
+</script>
+<style scoped></style>
