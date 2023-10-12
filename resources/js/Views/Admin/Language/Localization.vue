510f1e7aa04767df1be4c9ccdcf681c9da2776ed
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-10-12 12:15:41 +0300

    Fix duplicate var name bug - The props and local variable had identical name which made the eslint exception

--- resources/js/Views/Admin/Language/Localization.vue
+++ resources/js/Views/Admin/Language/Localization.vue
@@ -7,29 +7,33 @@
 </template>
 
 <script setup>
-import {computed, ref} from 'vue'
-import {router} from '@inertiajs/vue3'
+import { computed, ref } from "vue";
+import { router } from "@inertiajs/vue3";
 
 const props = defineProps({
-    currentLocale: {
+    locale: {
         type: String,
-        required: true
-    }
-})
+        required: true,
+    },
+});
 
-const currentLocale = ref('en')
+const currentLocale = ref("en");
 
 const selectableLocale = computed(() => {
-    return currentLocale.value === props.currentLocale ? 'en' : props.currentLocale;
-})
+    return currentLocale.value === props.locale ? "en" : props.locale;
+});
 
 const changeLocale = () => {
     const newLocale = selectableLocale.value;
-    router.post('/set-locale/' + newLocale, {}, {
-        preserveState: true,
-        onSuccess: () => {
-            currentLocale.value = newLocale;
+    router.post(
+        "/set-locale/" + newLocale,
+        {},
+        {
+            preserveState: true,
+            onSuccess: () => {
+                currentLocale.value = newLocale;
+            },
         }
-    })
+    );
 };
 </script>
