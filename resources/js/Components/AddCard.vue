548c470c19dccb9da66e161fbaa61d33ba6f7da0
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-10-12 10:30:02 +0300

    Update AddCard.vue component to accept title as props

--- resources/js/Components/AddCard.vue
+++ resources/js/Components/AddCard.vue
@@ -1,12 +1,22 @@
 <template>
     <div
-        class="flex w-fit cursor-pointer items-center justify-center rounded-xl bg-brand-100 p-4 shadow-sm hover:scale-105"
+        class="flex w-fit cursor-pointer flex-col items-center justify-center space-y-3 rounded-xl bg-brand-100 p-4 shadow-sm hover:scale-105"
     >
         <PlusCircleIcon class="w-12 text-brand-250" />
+        <div class="text-xs font-medium uppercase text-brand-500">
+            {{ title }}
+        </div>
     </div>
 </template>
 
 <script setup>
 import { PlusCircleIcon } from "@heroicons/vue/24/solid";
+
+defineProps({
+    title: {
+        type: String,
+        default: "",
+    },
+});
 </script>
 <style scoped></style>
