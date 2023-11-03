acdb6592ab0b252b7b8bf6336fad3fe991701f05
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-11-03 16:19:21 +0300

    Update assign homeroom modal

--- resources/js/Views/Teacher/Views/Homeroom/AssignHomeroom.vue
+++ resources/js/Views/Teacher/Views/Homeroom/AssignHomeroom.vue
@@ -1,9 +1,57 @@
 <template>
     <div
-        class="relative flex flex-col space-y-3 rounded-lg bg-white p-4 text-center"
+        class="relative flex flex-col space-y-5 rounded-lg bg-white p-4 text-center"
     >
-        <div>
-            <Title :title="$t('assignHomeroom.assignHomeroom')" />
+        <div><Title :title="$t('assignHomeroom.assignHomeroom')" /></div>
+
+        <div
+            v-if="selectedBatch && selectedBatch.value"
+            class="flex items-center justify-center rounded-lg p-3"
+        >
+            <div
+                v-if="selectedBatch.batch"
+                class="flex w-full flex-col items-center space-y-4 rounded-lg border-2 border-red-500 p-3 text-sm"
+            >
+                <span
+                    class="flex items-center justify-center space-x-1 text-xl font-bold"
+                >
+                    <ExclamationTriangleIcon class="w-6" />
+                    <span>Caution </span>
+                </span>
+                <p class="font-medium">
+                    Do you want to replace
+                    <span
+                        class="-skew-x-3 bg-brand-100 px-2 font-bold italic group-hover:bg-brand-300"
+                    >
+                        {{ selectedBatch.homeroom }}
+                    </span>
+                    with
+                    <span
+                        class="-skew-x-3 bg-brand-100 px-2 font-bold italic group-hover:bg-brand-300"
+                    >
+                        {{ teacherName }}
+                    </span>
+                    for
+                    <span class="italic">
+                        {{ selectedBatch.batch }}
+                    </span>
+                    ?
+                </p>
+                <PrimaryButton
+                    title="Change Homeroom"
+                    class="bg-red-600"
+                    @click="assignHomeroom"
+                />
+            </div>
+
+            <div v-else>
+                <div>
+                    <PrimaryButton
+                        :title="`Assign ${props.teacherName} to ${selectedBatch.label}`"
+                        @click="assignHomeroom"
+                    />
+                </div>
+            </div>
         </div>
 
         <div class="mx-auto mt-10 flex w-full flex-col">
@@ -14,58 +62,34 @@
                     v-model="searchKey"
                     type="text"
                     placeholder="Search for a grade"
-                    class-style="focus:outline-none focus:ring-2 focus:ring-brand-100 focus:border-zinc-100 focus:border-2 rounded-lg border-gray-400 "
                 />
                 <div
                     v-for="(batch, index) in batchOptions"
                     v-show="searchKey.length"
                     :key="batch"
-                    class="cursor-pointer"
+                    class="group cursor-pointer hover:bg-brand-300 hover:text-white"
                     :class="
-                        index % 2 === 0
-                            ? 'bg-brand-50/50 px-10 hover:bg-brand-200'
-                            : 'bg-white px-10 hover:bg-brand-100'
+                        batch.value === selectedBatch?.value
+                            ? 'bg-brand-300 text-white hover:bg-brand-300'
+                            : index % 2 === 0
+                            ? 'bg-brand-50/50 px-10'
+                            : 'bg-white px-10'
                     "
                     @click="selectBatch(batch)"
                 >
-                    <p class="px-3 py-2">{{ batch.label }}</p>
-                </div>
-
-                <div
-                    v-if="selectedBatch && selectedBatch.value"
-                    class="absolute right-0 top-0 mx-3 flex w-4/12 items-center justify-center rounded"
-                >
-                    <div
-                        class="flex w-full gap-5 rounded border-b border-black py-3"
-                    >
-                        <div class="flex items-center justify-center">
-                            <div v-if="selectedBatch.label">
-                                <p class="text-sm font-semibold">
-                                    {{
-                                        selectedBatch.label
-                                            ? selectedBatch.label
-                                            : selectedBatch.value
-                                    }}
-                                </p>
-
-                                <div>
-                                    <PrimaryButton
-                                        :title="`Change to ${props.teacherName}`"
-                                        @click="assignHomeroom"
-                                    />
-                                </div>
-                            </div>
-
-                            <div v-else>
-                                <div>
-                                    <PrimaryButton
-                                        :title="`Assign ${props.teacherName} to ${selectedBatch.label}`"
-                                        @click="assignHomeroom"
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <p class="px-3 py-2 text-sm">
+                        Current Homeroom for {{ batch.batch }} is
+                        <span
+                            class="-skew-x-3 bg-brand-100 px-2 font-bold italic group-hover:bg-brand-300"
+                            :class="
+                                selectedBatch?.value === batch.value
+                                    ? 'bg-brand-300 '
+                                    : ''
+                            "
+                        >
+                            {{ batch.homeroom }}
+                        </span>
+                    </p>
                 </div>
             </div>
         </div>
@@ -79,6 +103,7 @@ import Title from "@/Views/Teacher/Views/Title.vue";
 import { debounce } from "lodash";
 import TextInput from "@/Components/TextInput.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { ExclamationTriangleIcon } from "@heroicons/vue/20/solid";
 
 const emit = defineEmits(["close"]);
 
@@ -102,15 +127,11 @@ const batches = computed(() => usePage().props.batches);
 const batchOptions = computed(() => {
     return batches?.value.map((batch) => {
         return {
-            label:
-                (batch?.homeroom_teacher?.teacher
-                    ? batch?.homeroom_teacher?.teacher?.user.name
-                    : "-") +
-                " is assigned to " +
-                batch.level.name +
-                " " +
-                batch.section,
+            batch: batch.level.name + batch.section,
             value: batch.id,
+            homeroom: batch?.homeroom_teacher?.teacher
+                ? batch?.homeroom_teacher?.teacher?.user.name
+                : "-",
         };
     });
 });
