fb3a0653c8bf3c094e71bdbdc1bf90459e13f189
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-12-11 12:52:16 +0300

    Update import batch schedule style

--- resources/js/Views/Admin/GettingStarted/Schedule/BatchSchedule.vue
+++ resources/js/Views/Admin/GettingStarted/Schedule/BatchSchedule.vue
@@ -1,39 +1,35 @@
 <template>
     <div
-        class="container mx-auto flex h-full max-h-full w-10/12 flex-col space-y-10 px-2 pt-6 md:px-6 md:pt-6"
+        class="container mx-auto flex h-full max-h-full w-9/12 flex-col space-y-10 px-2 pt-6 md:px-6 md:pt-6"
     >
         <div class="w-11/12 text-4xl font-semibold">Import Class Schedules</div>
 
-        <span class="flex h-fit w-full items-center justify-between">
-            <span
-                class="flex h-fit w-4/12 flex-col justify-evenly space-y-12 py-10"
-            >
-                <span>
-                    <Heading value="Class Schedule Importer" />
-                    <Heading
-                        :value="$t('createStudent.headingFour')"
-                        size="sm"
-                        class="text-xs !font-light text-zinc-700"
-                    />
+        <span class="flex h-fit w-full items-center justify-between pt-5">
+            <span class="flex w-5/12 flex-col space-y-8 rounded-lg">
+                <span class="text-xl font-bold">* NOTE</span>
+
+                <span class="text-sm font-medium text-gray-700">
+                    Please be aware that importing a new class schedule will
+                    cancel any currently active schedules.
                 </span>
 
-                <span class="flex flex-col space-y-4">
-                    <span class="text-sm font-light">
-                        Here is a sample file for your reference. Please make
-                        sure to follow the format of the sample file.
-                    </span>
-                    <a
-                        class="flex items-center justify-center space-x-2 rounded-md border border-black bg-brand-400 px-3 py-1.5 text-center text-xs text-white opacity-100 transition-opacity duration-150 disabled:opacity-50"
-                        href="/assets/bulk_registration_samples/admin_bulk_registration_sample.xlsx"
-                        download
-                    >
-                        <ArrowDownCircleIcon class="w-4" />
-                        <span> Download Sample </span>
-                    </a>
+                <span class="text-sm font-medium text-gray-700">
+                    Here is a sample file for your reference. Please make sure
+                    to follow the format of the sample file. Make sure the file
+                    is in .xlsx, .xls, or .csv format.
                 </span>
+
+                <a
+                    class="flex !w-fit items-center justify-center space-x-2 rounded-md border border-black bg-brand-400 px-3 py-1.5 text-center text-xs text-white opacity-100 transition-opacity duration-150 disabled:opacity-50"
+                    href="/assets/bulk_registration_samples/admin_bulk_registration_sample.xlsx"
+                    download
+                >
+                    <ArrowDownCircleIcon class="w-4" />
+                    <span> Download Sample </span>
+                </a>
             </span>
 
-            <span class="w-5/12">
+            <span class="w-6/12">
                 <FileInput
                     max-file-size="10000000"
                     @file-uploaded="handleFileUploaded"
@@ -54,7 +50,6 @@
 </template>
 
 <script setup>
-import Heading from "@/Components/Heading.vue";
 import TertiaryButton from "@/Components/TertiaryButton.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import { ArrowDownCircleIcon } from "@heroicons/vue/20/solid";
